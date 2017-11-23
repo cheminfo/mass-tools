d3aa0c834858b4b92cb6c3db837c022ec565e188
Luc Patiny <luc@patiny.com>
2017-11-23 14:05:18 +0100

    add style file

---
+++ src/Style.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    SUPERIMPOSE: 'flex-direction: column;display: inline-flex;justify-content: center;text-align: left;vertical-align: middle;',
+    SUPERIMPOSE_SUP_SUB: 'line-height: 1; font-size: 70%'
+};
+
