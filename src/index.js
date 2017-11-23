62ccfd75662e9e3f931159cca1b3284c057ca722
Luc Patiny <luc@patiny.com>
2017-11-23 08:49:39 +0100

    towards toHTML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const toDisplay = require('./util/toDisplay');
+const parse = require('./parse');
+const toHtml = require('./util/toHtml');
+
+function parseToHtml(mf) {
+    var parsed = parse(mf);
+    console.log(parsed);
+    var display = toDisplay(parsed);
+    console.log(display);
+    return toHtml(display);
+}
+
 module.exports = {
     Kind: require('./Kind'),
     Format: require('./Format'),
-    parse: require('./parse')
+    parse: require('./parse'),
+    toDisplay,
+    toHtml,
+    parseToHtml,
 };
--- src/util/toHtml.js
+++ src/util/toHtml.js
@@ -5,7 +5,6 @@ const Format = require('../Format');
 const STYLE_SUPERIMPOSE = 'flex-direction: column;display: inline-flex;justify-content: center;text-align: left;vertical-align: middle;';
 const STYLE_SUPERIMPOSE_SUP_SUB = 'line-height: 1; font-size: 70%';
 
-
 module.exports = function getHtml(lines) {
     var html = [];
     for (let line of lines) {
@@ -28,4 +27,3 @@ module.exports = function getHtml(lines) {
     }
     return html.join('');
 };
-
