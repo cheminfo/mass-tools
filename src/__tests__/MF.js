f93a29f2ab8fcc3201e181f7603cfbaaa20d54c7
Luc Patiny <luc@patiny.com>
2017-11-27 17:15:17 +0100

    add test case for toInfo

--- src/__tests__/MF.js
+++ src/__tests__/MF.js
@@ -5,6 +5,7 @@ var MF = require('../MF');
 test('MF', () => {
     var mf = new MF('Et3N.HCl');
     var parts = mf.toParts();
+
     expect(parts).toEqual(
         [[
             {kind: 'atom', value: 'C', multiplier: 6},
@@ -14,6 +15,16 @@ test('MF', () => {
             {kind: 'atom', value: 'Cl', multiplier: 1},
             {kind: 'atom', value: 'H', multiplier: 1}
         ]]
-    ); mf.canonize(); var toHtml = mf.toHtml();
-    expect(toHtml).toBe('C<sub>6</sub>H<sub>15</sub>N<sub>1</sub> • Cl<sub>1</sub>H<sub>1</sub>');
+    );
+
+    mf.canonize();
+    let html = mf.toHtml();
+
+    expect(html).toBe('C<sub>6</sub>H<sub>15</sub>N<sub>1</sub> • Cl<sub>1</sub>H<sub>1</sub>');
+
+    let info = mf.toInfo();
+    expect(info).toEqual({
+        mf: '',
+        parts: []
+    });
 });
