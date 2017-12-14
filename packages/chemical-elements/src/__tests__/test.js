8ed8dca3dacbfa22337ca7b56fc82ee4870077c7
Luc Patiny <luc@patiny.com>
2017-12-14 09:52:33 +0100

    rename __tests to __tests__

--- packages/chemical-elements/src/__tests__/test.js
+++ packages/chemical-elements/src/__tests__/test.js
@@ -1,7 +1,34 @@
-import myModule from '..';
+'use strict';
 
-describe('test myModule', () => {
-    it('should return 42', () => {
-        expect(myModule()).toEqual(42);
+var data = require('..');
+
+test('data size', () => {
+    expect(data.elements.length).toBe(118);
+    expect(data.groups.length).toBeGreaterThan(50);
+});
+
+
+test('getElementsObject', () => {
+    var elementsObject = data.getElementsObject();
+    expect(Object.keys(elementsObject).length).toBe(118);
+});
+
+test('getGroupsObject', () => {
+    var groupsObject = data.getGroupsObject();
+    expect(groupsObject.Ala).toEqual({
+        elements:
+            [
+                {number: 3, symbol: 'C'},
+                {number: 5, symbol: 'H'},
+                {number: 1, symbol: 'N'},
+                {number: 1, symbol: 'O'}
+            ],
+        mass: 71.07801959624871,
+        monoisotopicMass: 71.03711378515,
+        mf: 'C3H5NO',
+        name:
+        'Alainine diradical',
+        symbol: 'Ala'
     });
 });
+
