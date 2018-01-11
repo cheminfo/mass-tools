481f00c7d4eb1539902f33ea856e2b8d6c550553
Luc Patiny <luc@patiny.com>
2018-01-11 10:43:45 +0100

    add mf-matcher

---
+++ packages/mf-matcher/README.md
@@ -0,0 +1,28 @@
+# mf-matcher
+
+  [![NPM version][npm-image]][npm-url]
+  [![npm download][download-image]][download-url]
+
+Filter an array of object using mw, em, msem, unsaturation and atoms.
+
+## Installation
+
+`$ npm install --save mf-matcher`
+
+## Usage
+
+```js
+import library from 'mf-matcher';
+
+const result = library(args);
+// result is ...
+```
+
+## License
+
+  [MIT](./LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/mf-matcher.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/mf-matcher
+[download-image]: https://img.shields.io/npm/dm/mf-matcher.svg?style=flat-square
+[download-url]: https://www.npmjs.com/package/mf-matcher
---
+++ packages/mf-matcher/src/index.js
@@ -0,0 +1,53 @@
+/**
+ * Returns a very important number
+ * @return {boolean}
+ */
+module.exports= function mfFilter(entry, options = {}) {
+    const {
+        minMW = 0,
+        maxMW = +Infinity,
+        minEM = 0,
+        maxEM = +Infinity,
+        minMSEM = 0,
+        maxMSEM = +Infinity,
+        minCharge = -Infinity,
+        maxCharge = +Infinity,
+        minUnsaturation = -Infinity,
+        maxUnsaturation = +Infinity,
+        onlyIntegerUnsaturation,
+        onlyNonIntegerUnsaturation,
+        atoms
+    } = options;
+     
+    if (entry.mw !== undefined) {
+        if ((entry.mw < minMW) || (entry.mw > maxMW)) return false;
+    }
+    
+    if (entry.em !== undefined) {
+        if ((entry.em < minEM) || (entry.em > maxEM)) return false;
+    }
+
+    if (entry.msem !== undefined) {
+        if ((entry.msem < minMSEM) || (entry.msem > maxMSEM)) return false;
+    }
+
+    if (entry.charge !== undefined) {
+        if ((entry.charge < minCharge) || (entry.charge > maxCharge)) return false;
+    }
+
+    if (entry.unsaturation !== undefined) {
+        if (entry.unsaturation<minUnsaturation || entry.unsaturation > maxUnsaturation) return;
+        if (onlyIntegerUnsaturation && ! Number.isInteger(entry.unsaturation)) return;
+        if (onlyNonIntegerUnsaturation && Number.isInteger(entry.unsaturation)) return;
+    }
+
+    if (entry.atoms !== undefined && atoms) {
+        // all the atoms of the entry must fit in the range
+        for (let atom of Object.keys(entry.atoms)) {
+            if (! atoms[atom]) return false;
+            if (entry.atoms[atom]<atoms[atom].min) return false;
+            if (entry.atoms[atom]>atoms[atom].max) return false;
+        }
+    }
+    return true;
+}
