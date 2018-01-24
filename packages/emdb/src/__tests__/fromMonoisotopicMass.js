23df48c55124ba21813502b1b9bf2a9dbae8933c
Luc Patiny <luc@patiny.com>
2018-01-24 13:45:45 +0100

    add more test on fromMonoisotopicMass

--- packages/emdb/src/__tests__/fromMonoisotopicMass.js
+++ packages/emdb/src/__tests__/fromMonoisotopicMass.js
@@ -18,3 +18,16 @@ test('test fromMonoisotopicMass with modifications', () => {
     });
     expect(dbManager.databases.monoisotopic).toHaveLength(9);
 });
+
+test('test fromMonoisotopicMass large database', () => {
+    let dbManager = new DBManager();
+    dbManager.fromMonoisotopicMass(1000, {
+        ranges: 'C0-100 H0-100 N0-100 O0-100',
+        minUnsaturation: 0,
+        maxUnsaturation: 100,
+        precision: 100,
+        onlyIntegerUnsaturation: true,
+        allowNeutral: true,
+    });
+    expect(dbManager.databases.monoisotopic).toHaveLength(1407);
+});
