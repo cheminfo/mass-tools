249d40f3f471adebd5b165563c7f992cf624e71d
Luc Patiny <luc@patiny.com>
2018-01-31 16:53:01 +0100

    add getClosestEntry

--- packages/number-treemap/src/TreeMap.js
+++ packages/number-treemap/src/TreeMap.js
@@ -23,6 +23,32 @@ class TreeMap {
         return null;
     }
 
+    getClosestEntry(key) {
+        let currentNode = this.root;
+        let bestMatch = currentNode;
+        let bestDistance = Number.POSITIVE_INFINITY;
+        while (currentNode != null) {
+            let distance = Math.abs(currentNode.key - key);
+            if (key < currentNode.key) {
+                if (distance < bestDistance) {
+                    bestMatch = currentNode;
+                    bestDistance = distance;
+                }
+                currentNode = currentNode.left;
+            } else if (key > currentNode.key) {
+                if (distance < bestDistance) {
+                    bestMatch = currentNode;
+                    bestDistance = distance;
+                }
+                currentNode = currentNode.right;
+            } else {
+                return currentNode;
+            }
+        }
+        return bestMatch;
+
+    }
+
     set(key, value) {
         if (this.root === null) {
             this.root = new Node(key, value);
