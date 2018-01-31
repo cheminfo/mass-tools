55cfdfaa0c7c6bbea15e9a2f7c9b74725cb9ef2d
Luc Patiny <luc@patiny.com>
2018-01-31 17:10:50 +0100

    add getFirstEntry and getLastEntry

--- packages/number-treemap/src/TreeMap.js
+++ packages/number-treemap/src/TreeMap.js
@@ -1,19 +1,16 @@
 'use strict';
 
-const Node = require('./Node');
-
-
 class TreeMap {
     constructor() {
         this.root = null;
         this.size = 0;
     }
-
-
 }
 
 TreeMap.prototype.get = require('./get.js');
 TreeMap.prototype.set = require('./set.js');
 TreeMap.prototype.getClosestEntry = require('./getClosestEntry.js');
+TreeMap.prototype.getFirstEntry = require('./getFirstEntry.js');
+TreeMap.prototype.getLastEntry = require('./getLastEntry.js');
 
 module.exports = TreeMap;
---
+++ packages/number-treemap/src/getLastEntry.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function getLastEntry() {
+    let currentNode = this.root;
+    if (currentNode !== null) {
+        while (currentNode.right !== null) {
+            currentNode = currentNode.right;
+        }
+    }
+    return currentNode;
+};
