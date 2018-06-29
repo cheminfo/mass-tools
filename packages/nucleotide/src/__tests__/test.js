4c8819f213e704ea3dc1dfa3dbae6c098838fb78
Luc Patiny <luc@patiny.com>
2018-06-29 06:47:32 +0200

    move nucleotide to separate project

--- packages/nucleotide/src/__tests__/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Nucleotide = require('..');
-
-describe('test nucleotide', () => {
-    test('sequenceToMF of AAA return ', () => {
-        expect(Nucleotide.sequenceToMF('AAA')).toEqual('HODampDampDampH');
-    });
-});
