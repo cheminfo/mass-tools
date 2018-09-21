edabff6d632800e21d2f743b9bbd0e251192f830
Luc Patiny <luc@patiny.com>
2018-09-21 15:23:26 +0200

    add ds-dna test case

--- packages/nucleotide/src/__tests__/test.js
+++ packages/nucleotide/src/__tests__/test.js
@@ -3,25 +3,29 @@
 const Nucleotide = require('..');
 
 describe('test nucleotide', () => {
-    test('sequenceToMF of ATC ', () => {
-        expect(Nucleotide.sequenceToMF('ATC')).toEqual(
-            'HOAmpTmpCmpH.HOGmpAmpTmpH'
-        );
-    });
+  test('sequenceToMF of ATC ', () => {
+    expect(Nucleotide.sequenceToMF('ATC')).toEqual('HOAmpTmpCmpH.HOGmpAmpTmpH');
+  });
 
-    test('sequenceToMF of AAU ', () => {
-        expect(Nucleotide.sequenceToMF('AAU')).toEqual('HOAmpAmpUmpH');
-    });
+  test('sequenceToMF of AAU ', () => {
+    expect(Nucleotide.sequenceToMF('AAU')).toEqual('HOAmpAmpUmpH');
+  });
 
-    test('sequenceToMF of circular AAA ', () => {
-        expect(Nucleotide.sequenceToMF('AAA', { circular: true })).toEqual(
-            'AmpAmpAmp.TmpTmpTmp'
-        );
-    });
+  test('sequenceToMF of circular AAA ', () => {
+    expect(Nucleotide.sequenceToMF('AAA', { circular: true })).toEqual(
+      'AmpAmpAmp.TmpTmpTmp'
+    );
+  });
 
-    test('sequenceToMF of DNA AAA ', () => {
-        expect(Nucleotide.sequenceToMF('AAA', { kind: 'DNA' })).toEqual(
-            'HODampDampDampH'
-        );
-    });
+  test('sequenceToMF of DNA AAA ', () => {
+    expect(Nucleotide.sequenceToMF('AAA', { kind: 'DNA' })).toEqual(
+      'HODampDampDampH'
+    );
+  });
+
+  test('sequenceToMF of ds DNA AAA ', () => {
+    expect(Nucleotide.sequenceToMF('AAA', { kind: 'ds-DNA' })).toEqual(
+      'HODampDampDampH.HODtmpDtmpDtmpH'
+    );
+  });
 });
