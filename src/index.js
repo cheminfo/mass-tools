e4a3812c9f848cdb16d939d3356a567854e78ae5
Luc Patiny <luc@patiny.com>
2018-12-07 13:19:02 +0100

    export Groups and Elements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   EMDB: require('../packages/emdb'),
+  Groups: require('../packages/chemical-groups/src/groups'),
+  Elements: require('../packages/chemical-elements/src/elementsAndIsotopes'),
   IsotopicDistribution: require('../packages/isotopic-distribution'),
   MF: require('../packages/mf-parser/src/MF.js'),
   Peptide: require('peptide'),
