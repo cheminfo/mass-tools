5246f0042974bbba8ae25318086c1be672d6450f
Luc Patiny <luc@patiny.com>
2018-12-15 15:47:10 +0100

    add getTable to isotopic distribution

--- packages/isotopic-distribution/src/IsotopicDistribution.js
+++ packages/isotopic-distribution/src/IsotopicDistribution.js
@@ -12,23 +12,22 @@ const getMsInfo = require('mf-utilities/src/getMsInfo');
 const Distribution = require('./Distribution');
 
 /**
-    * An object containing two arrays
-    * @typedef {object} XY
-    * @property {Array<number>} x - The x array
-    * @property {Array<number>} y - The y array
-*/
-
+ * An object containing two arrays
+ * @typedef {object} XY
+ * @property {Array<number>} x - The x array
+ * @property {Array<number>} y - The y array
+ */
 
 class IsotopicDistribution {
   /**
-     * Class that manage isotopic distribution
-     * @param {string} mf - Molecular formula
-     * @param {object} [options={}]
-     * @param {string} [options.ionizations=''] - string containing a comma separated list of modifications
-     * @param {number} [options.fwhm=0.01] - Amount of Dalton under which 2 peaks are joined
-     * @param {number} [options.maxLines=5000] - Maximal number of lines during calculations
-     * @param {number} [options.minY=1e-8] - Minimal signal height during calculations
-     */
+   * Class that manage isotopic distribution
+   * @param {string} mf - Molecular formula
+   * @param {object} [options={}]
+   * @param {string} [options.ionizations=''] - string containing a comma separated list of modifications
+   * @param {number} [options.fwhm=0.01] - Amount of Dalton under which 2 peaks are joined
+   * @param {number} [options.maxLines=5000] - Maximal number of lines during calculations
+   * @param {number} [options.minY=1e-8] - Minimal signal height during calculations
+   */
 
   constructor(mf, options = {}) {
     this.ionizations = preprocessIonizations(options.ionizations);
@@ -36,22 +35,23 @@ class IsotopicDistribution {
     this.mfInfo = this.mf.getInfo();
     let parts = this.mfInfo.parts || [this.mfInfo];
     this.parts = [];
-    for (let partOriginal of parts) { // we calculate information for each part
+    for (let partOriginal of parts) {
+      // we calculate information for each part
       for (const ionization of this.ionizations) {
         let part = JSON.parse(JSON.stringify(partOriginal));
         part.em = part.monoisotopicMass; // TODO: To remove !!! we change the name !?
-        part.isotopesInfo = (new MF(part.mf)).getIsotopesInfo();
+        part.isotopesInfo = new MF(part.mf).getIsotopesInfo();
         part.confidence = 0;
         part.ionization = ionization;
         part.ms = getMsInfo(part, {
-          ionization,
+          ionization
         });
         this.parts.push(part);
       }
     }
     this.options = options;
-    this.fwhm = (options.fwhm === undefined) ? 0.01 : options.fwhm;
-    this.minY = (options.minY === undefined) ? 1e-8 : options.minY;
+    this.fwhm = options.fwhm === undefined ? 0.01 : options.fwhm;
+    this.minY = options.minY === undefined ? 1e-8 : options.minY;
     this.maxLines = options.maxLines || 5000;
   }
 
@@ -60,13 +60,13 @@ class IsotopicDistribution {
   }
 
   /**
-     * @return {Distribution} returns the total distribution (for all parts)
-     */
+   * @return {Distribution} returns the total distribution (for all parts)
+   */
   getDistribution() {
     let options = {
       maxLines: this.maxLines,
       minY: this.minY,
-      deltaX: this.fwhm,
+      deltaX: this.fwhm
     };
     let finalDistribution;
     this.confidence = 0;
@@ -81,21 +81,27 @@ class IsotopicDistribution {
           totalDistribution.multiply(isotopeDistribution, options);
         }
       }
-      this.confidence += totalDistribution.array.reduce((sum, value) => (sum + value.y), 0);
+      this.confidence += totalDistribution.array.reduce(
+        (sum, value) => sum + value.y,
+        0
+      );
 
       // we finally deal with the charge
       let charge = part.isotopesInfo.charge + part.ionization.charge;
       let absoluteCharge = Math.abs(charge);
       if (charge) {
         totalDistribution.array.forEach((e) => {
-          e.x = (e.x + part.ionization.em - ELECTRON_MASS * charge) / absoluteCharge;
+          e.x =
+            (e.x + part.ionization.em - ELECTRON_MASS * charge) /
+            absoluteCharge;
         });
       }
 
       if (totalDistribution.array) {
         totalDistribution.sortX();
         part.fromX = totalDistribution.array[0].x;
-        part.toX = totalDistribution.array[totalDistribution.array.length - 1].x;
+        part.toX =
+          totalDistribution.array[totalDistribution.array.length - 1].x;
       }
 
       part.isotopicDistribution = totalDistribution.array;
@@ -111,7 +117,6 @@ class IsotopicDistribution {
     return finalDistribution;
   }
 
-
   getCSV() {
     return this.getText(',');
   }
@@ -120,19 +125,25 @@ class IsotopicDistribution {
     return this.getText('\t');
   }
 
-  getText(delimiter) {
+  getTable() {
+    return this.getDistribution().array;
+  }
+
+  getText(delimiter = '\t') {
     let points = this.getDistribution().array;
     let csv = [];
     for (let point of points) {
-      csv.push(`${point.x.toFixed(5)}${delimiter}${(point.y * 100).toFixed(3)}`);
+      csv.push(
+        `${point.x.toFixed(5)}${delimiter}${(point.y * 100).toFixed(3)}`
+      );
     }
     return csv.join('\n');
   }
 
   /**
-     * Returns the isotopic distirubtion
-     * @return {XY} an object containing 2 properties: x:[] and y:[]
-     */
+   * Returns the isotopic distirubtion
+   * @return {XY} an object containing 2 properties: x:[] and y:[]
+   */
   getXY() {
     let points = this.getDistribution().array;
     if (points.length === 0) return [];
@@ -149,9 +160,9 @@ class IsotopicDistribution {
   }
 
   /**
-     * Returns the isotopic distirubtion as the sum of gaussians
-     *  @return {XY} isotopic distribution as an object containing 2 properties: x:[] and y:[]
-     */
+   * Returns the isotopic distirubtion as the sum of gaussians
+   *  @return {XY} isotopic distribution as an object containing 2 properties: x:[] and y:[]
+   */
 
   getGaussian(options = {}) {
     let distribution = this.getDistribution();
