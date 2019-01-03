5e64f7cd9fc1c5a8fb33dc72c5dcbccbadf7d0f3
Luc Patiny <luc@patiny.com>
2019-01-03 12:37:45 +0100

    add capitalizeMF in mf-utilities

---
+++ packages/mf-utilities/src/__tests__/capitalizeMF.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const capitalizeMF = require('../capitalizeMF');
+
+const tests = [
+  ['triethylamine', 'triethylamine'],
+  ['ac1234', 'ac1234'],
+  ['CDCl3', 'CDCl3'],
+  ['CdCl2', 'CdCl2'],
+  ['kmno4', 'KMnO4'],
+  ['cdcl3', 'CDCl3'],
+  ['na2co3', 'Na2CO3'],
+  ['h2so4', 'H2SO4'],
+  ['Et3N', 'Et3N'],
+  ['h2o', 'H2O'],
+  ['ch2cl2', 'CH2Cl2'],
+  ['ch3cl', 'CH3Cl'],
+  ['ccl4', 'CCl4'],
+  ['chcl3', 'CHCl3'],
+  ['co2', 'CO2']
+];
+
+describe('capitalizeMF', () => {
+  test.each(tests)('%s', (mf, value) => {
+    expect(capitalizeMF(mf)).toBe(value);
+  });
+});
---
+++ packages/mf-utilities/src/capitalizeMF.js
@@ -0,0 +1,67 @@
+'use strict';
+
+let elements = require('chemical-elements').getElementsObject();
+let groups = require('chemical-groups').getGroupsObject();
+
+const isMF = require('./isMF');
+
+/**
+ * Try to capitalize a molecular formula based on what end users 'would' expect
+ */
+
+module.exports = function capitalizeMF(mf) {
+  if (!mf.match(/^[0-9a-zA-Z ]+/)) return mf;
+  if (isMF(mf)) return mf;
+  var oldMF = mf
+    .replace(/co/, 'C O')
+    .replace(/mn/, ' Mn ')
+    .replace(/cd/, ' C D ')
+    .replace(/([0-9])([a-zA-Z])/, '$1 $2');
+  var newMF = '';
+  var parts = oldMF.split(/ +/);
+
+  for (var i = 0; i < parts.length; i++) {
+    if (parts[i] === '') continue;
+    var label = parts[i].replace(/[0-9]*$/, '');
+    var number = parts[i].replace(/^[a-zA-Z]*/, '');
+
+    var casedLabel = getCasedLabel(label);
+
+    if (casedLabel) {
+      newMF += casedLabel + number;
+    } else {
+      return mf;
+    }
+  }
+  return newMF;
+};
+
+function getCasedLabel(label) {
+  if (elements[label]) return label;
+  if (groups[label]) return label;
+
+  if (label === '') return label;
+
+  var casedLabel =
+    label.substr(0, 1).toUpperCase() + label.substr(1).toLowerCase();
+  if (elements[label]) return casedLabel;
+  if (groups[label]) return casedLabel;
+
+  // we would like to cover chcl3, ch2o, ch2cl2, ccl4
+  var newLabel = label
+    .toLowerCase()
+    .replace(/cl/g, 'Cl')
+    .replace(/br/g, 'Br')
+    .replace(/na/g, 'Na')
+    .replace(/li/g, 'Li')
+    .replace(/si/g, 'Si')
+    .replace(/cr/g, 'Cr')
+    .replace(/c/g, 'C')
+    .replace(/o/g, 'O')
+    .replace(/n/g, 'N')
+    .replace(/h/g, 'H')
+    .replace(/k/g, 'K')
+    .replace(/s/g, 'S');
+  if (isMF(newLabel)) return newLabel;
+  return label;
+}
