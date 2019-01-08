4689e96684dbd22d91ad3f84355eaccbfd25017e
Luc Patiny <luc@patiny.com>
2019-01-08 10:06:02 +0100

    Add test case from array of array

--- packages/emdb/src/__tests__/fromArray.js
+++ packages/emdb/src/__tests__/fromArray.js
@@ -7,3 +7,14 @@ test('test fromArray', () => {
   dbManager.fromArray(['C.N', 'N.O']);
   expect(dbManager.databases.generated).toHaveLength(4);
 });
+
+test('test fromArray of array', () => {
+  let dbManager = new DBManager();
+  dbManager.fromArray(['C.N', ['Cl0-1', 'Br0-1']]);
+  let result = dbManager
+    .get('generated')
+    .map((entry) => entry.mf)
+    .sort()
+    .join();
+  expect(result).toBe('BrN,C,CBr,CCl,ClN,N');
+});
