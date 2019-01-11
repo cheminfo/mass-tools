e72c53bf56a74a7dccdfe96d60e6c31d98ddc294
Luc Patiny <luc@patiny.com>
2019-01-11 09:44:05 +0100

    feat (ms-spectrum): allow some missing part in isContinuous if y is 0

--- packages/ms-spectrum/src/__tests__/isContinuous.js
+++ packages/ms-spectrum/src/__tests__/isContinuous.js
@@ -4,35 +4,50 @@ const isContinuous = require('../isContinuous');
 
 describe('test isContinuous', () => {
   it('to small', () => {
-    let data = { x: [] };
+    let data = { x: [], y: [] };
     for (let i = 0; i < 50; i++) {
       data.x.push(i / 20);
+      data.y.push(i + 1);
     }
     expect(isContinuous({ data })).toBe(false);
   });
 
   it('to separated', () => {
-    let data = { x: [] };
+    let data = { x: [], y: [] };
     for (let i = 0; i < 200; i++) {
       data.x.push(i / 5);
+      data.y.push(i + 1);
     }
     expect(isContinuous({ data })).toBe(false);
   });
 
   it('bad points', () => {
-    let data = { x: [] };
+    let data = { x: [], y: [] };
     for (let i = 0; i < 200; i++) {
       data.x.push(i / 20);
+      data.y.push(i + 1);
     }
     data.x.push(200);
     expect(isContinuous({ data })).toBe(false);
   });
 
   it('just ok', () => {
-    let data = { x: [] };
+    let data = { x: [], y: [] };
     for (let i = 0; i < 200; i++) {
       data.x.push(i / 20);
+      data.y.push(i + 1);
     }
+    expect(isContinuous({ data })).toBe(true);
+  });
+
+  it('ok because < = 0', () => {
+    let data = { x: [], y: [] };
+    for (let i = 0; i < 200; i++) {
+      data.x.push(i / 20);
+      data.y.push(i / 20);
+    }
+    data.x.push(200);
+    data.y.push(0);
     expect(isContinuous({ data })).toBe(true);
   });
 });
--- packages/ms-spectrum/src/isContinuous.js
+++ packages/ms-spectrum/src/isContinuous.js
@@ -5,21 +5,28 @@
  * - has more than 100 points
  * - deltaX change can not be more than a factor 2
  * - deltaX may not be larger than 0.1
+ * - if y is zero it does not count
  * @param {*} spectrum
  */
 
 function isContinuous(spectrum) {
   if (spectrum.continuous === undefined) {
     let xs = spectrum.data.x;
+    let ys = spectrum.data.y;
     if (xs.length < 100) {
       spectrum.continuous = false;
     } else {
       let previousDelta = xs[1] - xs[0];
       spectrum.continuous = true;
+
       for (let i = 0; i < xs.length - 1; i++) {
         let delta = xs[i + 1] - xs[i];
         let ratio = delta / previousDelta;
-        if (Math.abs(delta) > 0.1 || ratio < 0.5 || ratio > 2) {
+        if (
+          (Math.abs(delta) > 0.1 || ratio < 0.5 || ratio > 2) &&
+          ys[i] !== 0 &&
+          ys[i + 1] !== 0
+        ) {
           spectrum.continuous = false;
           break;
         }
