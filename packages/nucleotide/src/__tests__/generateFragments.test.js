41d6a71ea00bc5f815db4882b4473bf2751bd5eb
Luc Patiny <luc@patiny.com>
2019-01-15 13:48:55 +0100

    update nucleic fragmentation

--- packages/nucleotide/src/__tests__/generateFragments.test.js
+++ packages/nucleotide/src/__tests__/generateFragments.test.js
@@ -5,13 +5,17 @@ const { generateFragments } = require('..');
 describe('test generate fragments', () => {
   it('nucleotide to sequence of OHDampDtmpDcmpH', () => {
     let fragments = generateFragments('HODampDtmpDcmpH', { a: true });
-    expect(fragments).toMatchSnapshot();
+    expect(fragments).toStrictEqual(['HODampO-1H-1$a1', 'HODampDtmpO-1H-1$a2']);
   });
   it('nucleotide to sequence of HODampDtmpDcmpH', () => {
     let fragments = generateFragments('HODampDtmpDcmpH', { a: true, ab: true });
-    expect(fragments).toMatchSnapshot();
+    expect(fragments).toStrictEqual([
+      'HODampO-1H-1$a1',
+      'HC5H6O4P$a1 - B',
+      'HODampDtmpO-1H-1$a2',
+      'HODampC5H6O4P$a2 - B'
+    ]);
   });
-
   it('nucleotide to sequence of HODampDtmpDcmpH and many fragments', () => {
     let fragments = generateFragments('HODampDtmpDcmpH', {
       a: true,
@@ -24,6 +28,36 @@ describe('test generate fragments', () => {
       y: true,
       z: true
     });
-    expect(fragments).toMatchSnapshot();
+    expect(fragments).toStrictEqual([
+      'HODampO-1H-1$a1',
+      'HC5H6O4P$a1 - B',
+      'HODamp$bH-11',
+      'HODampPO2$c1',
+      'HODampPO3H2$d1',
+      'HODcmpH$w1',
+      'H-2DcmpH$x1',
+      'O-2H-1P-1DcmpH$y1',
+      'O-3H-4P-1DcmpH$z1',
+      'HODampDtmpO-1H-1$a2',
+      'HODampC5H6O4P$a2 - B',
+      'HODampDtmp$bH-12',
+      'HODampDtmpPO2$c2',
+      'HODampDtmpPO3H2$d2',
+      'HODtmpDcmpH$w2',
+      'H-2DtmpDcmpH$x2',
+      'O-2H-1P-1DtmpDcmpH$y2',
+      'O-3H-4P-1DtmpDcmpH$z2'
+    ]);
+  });
+
+  it('nucleotide HODampDtmpDcmpH internal aby fragments', () => {
+    let fragments = generateFragments('HODampDtmpDcmpDgmpDampH', {
+      abw: true
+    });
+    expect(fragments).toStrictEqual([
+      'HODtmpC5H6O4P$B2:B3',
+      'HODtmpDcmpC5H6O4P$B2:B4',
+      'HODcmpC5H6O4P$B3:B4'
+    ]);
   });
 });
--- packages/nucleotide/src/generateFragments.js
+++ packages/nucleotide/src/generateFragments.js
@@ -13,7 +13,8 @@ module.exports = function (mf, options) {
       w: false,
       x: false,
       y: false,
-      z: false
+      z: false,
+      abw: false
     };
   }
 
@@ -26,11 +27,21 @@ module.exports = function (mf, options) {
   var fiveTerm = '';
   var threeTerm = '';
 
-  for (var i = 1; i < mfparts.length; i++) {
+  for (let i = 1; i < mfparts.length; i++) {
     fiveTerm += mfparts[i - 1];
     threeTerm = mfparts[mfparts.length - i] + threeTerm;
     addFiveTerm(mfs, fiveTerm, i, options);
     addThreeTerm(mfs, threeTerm, i, options);
+  }
+
+  for (let i = 1; i < mfparts.length - 1; i++) {
+    let internal = '';
+    for (var j = i; j < mfparts.length - 1; j++) {
+      internal += mfparts[j];
+      if (j > i) {
+        addInternalTerm(mfs, internal, i, j, options);
+      }
+    }
   }
 
   if (mfs.length === 0) {
@@ -41,16 +52,25 @@ module.exports = function (mf, options) {
 };
 
 function addFiveTerm(mfs, fiveTerm, i, options) {
-  if (options.a) mfs.push(`${fiveTerm}O-1$a${i}`);
+  if (options.a) mfs.push(`${fiveTerm}O-1H-1$a${i}`);
   if (options.ab) mfs.push(`${furanThreeTerm(fiveTerm)}$a${i} - B`);
-  if (options.b) mfs.push(`${fiveTerm}$b${i}`);
-  if (options.c) mfs.push(`${fiveTerm}PO2H$c${i}`);
-  if (options.d) mfs.push(`${fiveTerm}PO3H$d${i}`);
+  if (options.b) mfs.push(`${fiveTerm}$bH-1${i}`);
+  if (options.c) mfs.push(`${fiveTerm}PO2$c${i}`);
+  if (options.d) mfs.push(`${fiveTerm}PO3H2$d${i}`);
 }
 
 function addThreeTerm(mfs, threeTerm, i, options) {
-  if (options.w) mfs.push(`O${threeTerm}$w${i}`);
-  if (options.x) mfs.push(`${threeTerm}$x${i}`);
+  if (options.w) mfs.push(`HO${threeTerm}$w${i}`);
+  if (options.x) mfs.push(`H-2${threeTerm}$x${i}`);
   if (options.y) mfs.push(`O-2H-1P-1${threeTerm}$y${i}`);
-  if (options.z) mfs.push(`O-3H-2P-1${threeTerm}$z${i}`);
+  if (options.z) mfs.push(`O-3H-4P-1${threeTerm}$z${i}`);
+}
+
+// https://books.google.ch/books?id=B57e37bJjqAC&pg=PA172&lpg=PA172&dq=oligonucleotide+b+fragmentation&source=bl&ots=mRr29Pexx2&sig=1NUQcWV-wuj6o9q81my86AVoRto&hl=fr&sa=X&ved=2ahUKEwjI5M3yn-7fAhUJMewKHQR6Bcs4ChDoATADegQIBhAB#v=onepage&q=oligonucleotide%20b%20fragmentation&f=false
+
+function addInternalTerm(mfs, internal, i, j, options = {}) {
+  let fragment = furanThreeTerm(internal);
+  if (options.abw) {
+    mfs.push(`HO${fragment}$B${i + 1}:B${j + 1}`);
+  }
 }
