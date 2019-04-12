27c87098bec0e84b549e0bd1f07525a6ce4c67ce
Luc Patiny <luc@patiny.com>
2019-04-12 06:49:23 +0200

    fix nucleotide fragment Y and Z

--- packages/nucleotide/src/generateFragments.js
+++ packages/nucleotide/src/generateFragments.js
@@ -61,11 +61,13 @@ function addFiveTerm(mfs, fiveTerm, i, options) {
   if (options.dh2o) mfs.push(`${fiveTerm}PO2$d-H2O${i}`);
 }
 
+// doi:10.1016/j.jasms.2006.05.018
+
 function addThreeTerm(mfs, threeTerm, i, options) {
   if (options.w) mfs.push(`HO${threeTerm}$w${i}`);
   if (options.x) mfs.push(`H-2${threeTerm}$x${i}`);
-  if (options.y) mfs.push(`O-2H-1P-1${threeTerm}$y${i}`);
-  if (options.z) mfs.push(`O-3H-4P-1${threeTerm}$z${i}`);
+  if (options.y) mfs.push(`O-2P-1${threeTerm}$y${i}`);
+  if (options.z) mfs.push(`O-3H-1P-1${threeTerm}$z${i}`);
 }
 
 // https://books.google.ch/books?id=B57e37bJjqAC&pg=PA172&lpg=PA172&dq=oligonucleotide+b+fragmentation&source=bl&ots=mRr29Pexx2&sig=1NUQcWV-wuj6o9q81my86AVoRto&hl=fr&sa=X&ved=2ahUKEwjI5M3yn-7fAhUJMewKHQR6Bcs4ChDoATADegQIBhAB#v=onepage&q=oligonucleotide%20b%20fragmentation&f=false
