2c4b4e637ebf8aa9f4358679432e7d8d3d40f256
Luc Patiny <luc@patiny.com>
2019-10-14 10:14:05 +0200

    chore: add testcase for modified nucleotide

--- packages/nucleotide/src/__tests__/generateFragments.test.js
+++ packages/nucleotide/src/__tests__/generateFragments.test.js
@@ -3,23 +3,32 @@
 const { generateFragments } = require('..');
 
 describe('test generate fragments', () => {
-  it('nucleotide to sequence of OHDampDtmpDcmpH', () => {
+  it('nucleotide to sequence of HODampDtmpDcmpH', () => {
     let fragments = generateFragments('HODampDtmpDcmpH', { a: true });
     expect(fragments).toStrictEqual(['HODampO-1H-1$a1', 'HODampDtmpO-1H-1$a2']);
   });
-  it('nucleotide to sequence of HODampDtmpDcmpH', () => {
+
+  it('nucleotide to sequence internal modification of HODamp(NH2)DtmpDcmpH', () => {
+    let fragments = generateFragments('HODamp(NH2)DtmpDcmpH', { a: true });
+    expect(fragments).toStrictEqual([
+      'HODamp(NH2)O-1H-1$a1',
+      'HODamp(NH2)DtmpO-1H-1$a2'
+    ]);
+  });
+
+  it('nucleotide to sequence HODampDtmpDcmpH', () => {
     let fragments = generateFragments('HODampDtmpDcmpH', { a: true, ab: true });
     expect(fragments).toStrictEqual([
       'HODampO-1H-1$a1',
       'HOC5H6O4P$a1 - B',
       'HODampDtmpO-1H-1$a2',
-      'HODampC5H6O4P$a2 - B',
+      'HODampC5H6O4P$a2 - B'
     ]);
   });
 
   it('nucleotide HODampDtmpDcmpH dh2o (d with loss of water)', () => {
     let fragments = generateFragments('HODampDampH', {
-      dh2o: true,
+      dh2o: true
     });
     expect(fragments).toStrictEqual(['HODampPO2$d-H2O1']);
   });
@@ -34,7 +43,7 @@ describe('test generate fragments', () => {
       w: true,
       x: true,
       y: true,
-      z: true,
+      z: true
     });
     expect(fragments).toStrictEqual([
       'HODampO-1H-1$a1',
@@ -54,7 +63,7 @@ describe('test generate fragments', () => {
       'HODtmpDcmpH$w2',
       'H-1DtmpDcmpH$x2',
       'O-2P-1DtmpDcmpH$y2',
-      'O-3H-2P-1DtmpDcmpH$z2',
+      'O-3H-2P-1DtmpDcmpH$z2'
     ]);
   });
 });
