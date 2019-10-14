1d29b4e96743240b4f61338032d23a228d1ce664
Luc Patiny <luc@patiny.com>
2019-10-14 10:57:09 +0200

    working on internal fragments

--- packages/nucleotide/src/__tests__/generateFragments.internal.test.js
+++ packages/nucleotide/src/__tests__/generateFragments.internal.test.js
@@ -5,18 +5,18 @@ const { generateFragments } = require('..');
 describe('test generate internal fragments', () => {
   it('nucleotide HODampDtmpDcmpDgmpDampH internal abw fragments', () => {
     let fragments = generateFragments('HODampDtmpDcmpDgmpDampH', {
-      abw: true,
+      abw: true
     });
     expect(fragments).toStrictEqual([
       'HODtmpC5H6O4P$w4:a-B3',
       'HODtmpDcmpC5H6O4P$w4:a-B4',
-      'HODcmpC5H6O4P$w3:a-B4',
+      'HODcmpC5H6O4P$w3:a-B4'
     ]);
   });
 
   it('nucleotide HODampDtmpDcmpH internal aby fragments', () => {
     let fragments = generateFragments('HODthyDcmpDcmpDgmpDtmpDcmpH', {
-      aby: true,
+      aby: true
     });
     expect(fragments).toStrictEqual([
       'O-2P-1DcmpC5H6O4P$y5:a-B3',
@@ -24,7 +24,7 @@ describe('test generate internal fragments', () => {
       'O-2P-1DcmpDcmpDgmpC5H6O4P$y5:a-B5',
       'O-2P-1DcmpC5H6O4P$y4:a-B4',
       'O-2P-1DcmpDgmpC5H6O4P$y4:a-B5',
-      'O-2P-1DgmpC5H6O4P$y3:a-B5',
+      'O-2P-1DgmpC5H6O4P$y3:a-B5'
     ]);
   });
 
@@ -32,31 +32,38 @@ describe('test generate internal fragments', () => {
     let fragments = generateFragments(
       'HODthyDampDcmpDgmpDtmpDgmpDcmpDcmpDampDampDtmpDampDcmpH',
       {
-        abw: true,
-      },
+        abw: true
+      }
     );
     expect(fragments).toMatchSnapshot();
   });
 
   it('nucleotide HODampDtmpDcmpH internal aw fragments', () => {
     let fragments = generateFragments('HODampDtmpDcmpDgmpDampH', {
-      aw: true,
+      aw: true
     });
     expect(fragments).toStrictEqual([
       'HODtmpDcmpO-1H-1$w4:a3',
       'HODtmpDcmpDgmpO-1H-1$w4:a4',
-      'HODcmpDgmpO-1H-1$w3:a4',
+      'HODcmpDgmpO-1H-1$w3:a4'
     ]);
   });
 
   it('nucleotide HODampDtmpDcmpH internal bw fragments', () => {
     let fragments = generateFragments('HODampDtmpDcmpDgmpDampH', {
-      bw: true,
+      bw: true
     });
     expect(fragments).toStrictEqual([
       'HODtmpDcmpH$w4:b3',
       'HODtmpDcmpDgmpH$w4:b4',
-      'HODcmpDgmpH$w3:b4',
+      'HODcmpDgmpH$w3:b4'
     ]);
+  });
+
+  it('nucleotide to sequence internal modification of HODamp(NH2)DtmpDcmpH with internal fragments', () => {
+    let fragments = generateFragments('HODampDampDtmp(NH2)DampH', {
+      abw: true
+    });
+    expect(fragments).toStrictEqual(['HODampC5H6O4P$w3:a-B3']);
   });
 });
--- packages/nucleotide/src/furanThreeTerm.js
+++ packages/nucleotide/src/furanThreeTerm.js
@@ -4,13 +4,13 @@ function furanThreeTerm(nucleotide) {
   // last residue should become a furan
   let parts = nucleotide
     .replace(/ /g, '')
-    .replace(/([a-zA-Z0-9)])(?=[A-Z])/g, '$1 ')
+    .replace(/([a-z)0-9])([A-Z][a-z](?=[a-z]))/g, '$1 $2')
     .split(/ /);
   let last = parts.pop();
   if (!last.match(/^D[atcg]mp/)) {
     // eslint-disable-next-line no-console
     console.warn(
-      `furanThreeTerm can not remove a non monophosphate nucleic acid: ${last}`,
+      `furanThreeTerm can not remove a non monophosphate nucleic acid: ${last}`
     );
   }
   return `${parts.join('')}C5H6O4P`;
