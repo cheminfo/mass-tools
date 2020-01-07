6f92da6ef01c2791180e66c2947d222379b83d48
Luc Patiny <luc@patiny.com>
2020-01-07 13:49:44 +0100

    fix: nucleotide fragment labels

--- packages/ms-report/src/__tests__/appendResidues.test.js
+++ packages/ms-report/src/__tests__/appendResidues.test.js
@@ -4,33 +4,38 @@ const appendResidues = require('../appendResidues');
 
 describe('appendResidues', () => {
   it('AA(Ph)A(Ph)A(Ts)A(Ph)U', () => {
-    let result = appendResidues('AA(Ph)A(Ph)A(Ts)A(Ph)U');
-    expect(result).toMatchSnapshot();
-    p;
+    let data = {};
+    appendResidues(data, 'AA(Ph)A(Ph)A(Ts)A(Ph)U');
+    expect(data).toMatchSnapshot();
   });
 
   it('HAlaAlaAlaOH', () => {
-    let result = appendResidues('HAlaAlaAlaOH');
-    expect(result).toMatchSnapshot();
+    let data = {};
+    appendResidues(data, 'HAlaAlaAlaOH');
+    expect(data).toMatchSnapshot();
   });
 
   it('HAlaAla(H-1OH)AlaOH', () => {
-    let result = appendResidues('HAlaAla(H-1OH)AlaOH');
-    expect(result).toMatchSnapshot();
+    let data = {};
+    appendResidues(data, 'HAlaAla(H-1OH)AlaOH');
+    expect(data).toMatchSnapshot();
   });
 
   it('H(+)AlaAla(H-1OH)AlaOH', () => {
-    let result = appendResidues('H(+)AlaAla(H-1OH)AlaOH');
-    expect(result).toMatchSnapshot();
+    let data = {};
+    appendResidues(data, 'H(+)AlaAla(H-1OH)AlaOH');
+    expect(data).toMatchSnapshot();
   });
 
   it('ForAlaAla(H-1OH)AlaOH', () => {
-    let result = appendResidues('(For)AlaAla(H-1OH)AlaOH');
-    expect(result).toMatchSnapshot();
+    let data = {};
+    appendResidues(data, '(For)AlaAla(H-1OH)AlaOH');
+    expect(data).toMatchSnapshot();
   });
 
   it('(MeO)AA(H-1NH2)AA rna', () => {
-    let result = appendResidues('(MeO)AA(H-1NH2)AA', { kind: 'rna' });
-    expect(result).toMatchSnapshot();
+    let data = {};
+    appendResidues(data, '(MeO)AA(H-1NH2)AA', { kind: 'rna' });
+    expect(data).toMatchSnapshot();
   });
 });
