b3b6b7520eee499c61e5c6c2f136110bb8e9964e
Luc Patiny <luc@patiny.com>
2020-01-07 13:37:41 +0100

    chore: rename appendResidues

--- packages/ms-report/src/__tests__/sequenceParser.test.js
+++ packages/ms-report/src/__tests__/sequenceParser.test.js
@@ -1,35 +1,36 @@
 'use strict';
 
-const sequenceParser = require('../sequenceParser');
+const appendResidues = require('../appendResidues');
 
-describe('sequenceParser', () => {
+describe('appendResidues', () => {
   it('AA(Ph)A(Ph)A(Ts)A(Ph)U', () => {
-    let result = sequenceParser('AA(Ph)A(Ph)A(Ts)A(Ph)U');
+    let result = appendResidues('AA(Ph)A(Ph)A(Ts)A(Ph)U');
     expect(result).toMatchSnapshot();
+    p;
   });
 
   it('HAlaAlaAlaOH', () => {
-    let result = sequenceParser('HAlaAlaAlaOH');
+    let result = appendResidues('HAlaAlaAlaOH');
     expect(result).toMatchSnapshot();
   });
 
   it('HAlaAla(H-1OH)AlaOH', () => {
-    let result = sequenceParser('HAlaAla(H-1OH)AlaOH');
+    let result = appendResidues('HAlaAla(H-1OH)AlaOH');
     expect(result).toMatchSnapshot();
   });
 
   it('H(+)AlaAla(H-1OH)AlaOH', () => {
-    let result = sequenceParser('H(+)AlaAla(H-1OH)AlaOH');
+    let result = appendResidues('H(+)AlaAla(H-1OH)AlaOH');
     expect(result).toMatchSnapshot();
   });
 
   it('ForAlaAla(H-1OH)AlaOH', () => {
-    let result = sequenceParser('(For)AlaAla(H-1OH)AlaOH');
+    let result = appendResidues('(For)AlaAla(H-1OH)AlaOH');
     expect(result).toMatchSnapshot();
   });
 
   it('(MeO)AA(H-1NH2)AA rna', () => {
-    let result = sequenceParser('(MeO)AA(H-1NH2)AA', { kind: 'rna' });
+    let result = appendResidues('(MeO)AA(H-1NH2)AA', { kind: 'rna' });
     expect(result).toMatchSnapshot();
   });
 });
