9b342125c24fd97e7acc9ef47df6aa823a9c74e0
Luc Patiny <luc@patiny.com>
2020-01-13 14:40:47 +0100

    chore: fix duplicate prefix in testcase

--- src/__tests__/moleculerFormula.test.js
+++ src/__tests__/moleculerFormula.test.js
@@ -2,25 +2,27 @@
 
 const { MF, IsotopicDistribution } = require('..');
 
-test('test emdbUtil isotopicDistrbution', () => {
-  let isotopicDistribution = new IsotopicDistribution('C10');
-  let xy = isotopicDistribution.getXY();
-  expect(xy.x[0]).toBe(120);
-  expect(xy.y[0]).toBe(100);
-});
+describe('molecular-formula', () => {
+  it('isotopicDistrbution', () => {
+    let isotopicDistribution = new IsotopicDistribution('C10');
+    let xy = isotopicDistribution.getXY();
+    expect(xy.x[0]).toBe(120);
+    expect(xy.y[0]).toBe(100);
+  });
 
-test('test emdbUtil MF', () => {
-  let mf = new MF('C10');
-  let html = mf.toHtml();
-  let info = mf.getInfo();
+  it('MF', () => {
+    let mf = new MF('C10');
+    let html = mf.toHtml();
+    let info = mf.getInfo();
 
-  expect(html).toBe('C<sub>10</sub>');
-  expect(info).toMatchObject({
-    mass: 120.10735896735248,
-    monoisotopicMass: 120,
-    charge: 0,
-    mf: 'C10',
-    atoms: { C: 10 },
-    unsaturation: 11,
+    expect(html).toBe('C<sub>10</sub>');
+    expect(info).toMatchObject({
+      mass: 120.10735896735248,
+      monoisotopicMass: 120,
+      charge: 0,
+      mf: 'C10',
+      atoms: { C: 10 },
+      unsaturation: 11,
+    });
   });
 });
