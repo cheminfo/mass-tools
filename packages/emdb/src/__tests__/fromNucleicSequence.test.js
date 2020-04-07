ad29d23bf12e20114bae3c16b53717373d7a038c
Luc Patiny <luc@patiny.com>
2020-04-07 12:06:28 +0200

    chore: remove snapshot testing

--- packages/emdb/src/__tests__/fromNucleicSequence.test.js
+++ packages/emdb/src/__tests__/fromNucleicSequence.test.js
@@ -1,4 +1,7 @@
 'use strict';
+const { toBeDeepCloseTo } = require('jest-matcher-deep-close-to');
+
+expect.extend({ toBeDeepCloseTo });
 
 const DBManager = require('..');
 
@@ -15,7 +18,18 @@ test('fromNucleicSequence', () => {
   });
   let nucleic = dbManager.databases.nucleic.sort((a, b) => a.ms.em - b.ms.em);
   expect(nucleic).toHaveLength(6);
-  expect(nucleic).toMatchSnapshot();
+  expect(nucleic[0]).toBeDeepCloseTo({
+    charge: 0,
+    em: 313.05760550518,
+    mw: 313.2069506932622,
+    ionization: { mf: 'H+', em: 1.00782503223, charge: 1 },
+    unsaturation: 8,
+    atoms: { C: 10, H: 12, N: 5, O: 5, P: 1 },
+    ms: { ionization: 'H+', em: 314.0648819575009, charge: 1 },
+    parts: ['HODampO-1H-1'],
+    mf: 'C10H12N5O5P',
+    comment: 'a1',
+  });
 });
 
 test('fromNucleicSequence ds-DNA', () => {
@@ -49,9 +63,19 @@ test('TACGTGCCAATAC internal fragment', () => {
   });
 
   let nucleic = dbManager.databases.nucleic.sort((a, b) => a.ms.em - b.ms.em);
-
   expect(nucleic).toHaveLength(56);
-  expect(nucleic).toMatchSnapshot();
+  expect(nucleic[0]).toBeDeepCloseTo({
+    charge: 0,
+    em: 467.04948243777,
+    mw: 467.2622646800317,
+    ionization: { mf: '(H+)-5', em: -5.03912516115, charge: -5 },
+    unsaturation: 8,
+    atoms: { C: 14, H: 19, N: 3, O: 11, P: 2 },
+    ms: { ionization: '(H+)-5', em: 92.40262003523306, charge: -5 },
+    parts: ['HODcmpC5H6O4P'],
+    mf: 'C14H19N3O11P2',
+    comment: 'w6:a9-B',
+  });
 });
 
 test('AGGCAG fragment', () => {
--- packages/emdb/src/__tests__/searchSimilarity.peptide.test.js
+++ packages/emdb/src/__tests__/searchSimilarity.peptide.test.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const { toBeDeepCloseTo } = require('jest-matcher-deep-close-to');
+
+expect.extend({ toBeDeepCloseTo });
+
 const fs = require('fs');
 const join = require('path').join;
 
@@ -44,9 +48,50 @@ describe('test searchSimilarity for peptide', () => {
         common: 'second', // 'first', 'second', 'both' (or true) or 'none' (or undefined)
       },
     });
-
-    expect(results).toMatchSnapshot();
-    expect(results.peptidic[0].ms.similarity.value).toBeCloseTo(0.96, 2);
+    delete results.peptidic[0].ms.similarity.experimental;
+    expect(results.peptidic[0]).toBeDeepCloseTo({
+      charge: 1,
+      em: 4210.304872186381,
+      mw: 4212.888237254821,
+      ionization: { mf: 'H2(2+)', em: 2.01565006446, charge: 2 },
+      unsaturation: 53,
+      atoms: { C: 188, H: 318, N: 47, O: 59, S: 1 },
+      ms: {
+        ionization: 'H2(2+)',
+        em: 1404.106292170371,
+        charge: 3,
+        similarity: {
+          value: 0.9646106703663371,
+          theoretical: [
+            [1404.1062921703708, 1404.4405986138634, 1404.774826084723],
+            [0.16643354388073694, 0.37817634643567843, 0.45539010968358457],
+          ],
+          difference: [
+            [1404.1062921703708, 1404.4405986138634, 1404.774826084723],
+            [0.021767122285728965, 0.01362220734793394, 0],
+          ],
+          experimentalInfo: {
+            sum: 0.0007671062364860218,
+            min: 0,
+            max: 0.00009697908619110793,
+            from: 1403.9396255037043,
+            to: 1404.9396255037043,
+          },
+          thereoticalInfo: {
+            sum: 0.5313057912545383,
+            min: 0.08842710572285185,
+            max: 0.24195140255492786,
+          },
+          quantity: 0.0007671062364860208,
+          width: { bottom: 0.1855132865212964, top: 0.09775664326064819 },
+        },
+      },
+      parts: [
+        'HMetGlnIlePheValLysThrLeuThrGlyLysThrIleThrLeuGluValGluProSerAspThrIleGluAsnValLysAlaLysIleGlnAspLysGluGlyIleProProNH3(+1)',
+      ],
+      mf: 'C188H318N47O59S(+1)',
+      comment: 'c38',
+    });
   });
 
   it('should find one result with bad distribution and string fucntion', () => {
@@ -82,7 +127,6 @@ describe('test searchSimilarity for peptide', () => {
       },
     });
 
-    expect(results).toMatchSnapshot();
     expect(results.peptidic[0].ms.similarity.value).toBeCloseTo(0.96, 2);
   });
 });
