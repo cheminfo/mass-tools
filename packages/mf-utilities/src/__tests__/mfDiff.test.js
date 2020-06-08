f082f57f8fe269116ea483b009697e89d3d67a33
Luc Patiny <luc@patiny.com>
2020-06-08 11:43:53 +0200

    feat: add mfDiff

---
+++ packages/mf-utilities/src/__tests__/mfDiff.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mfDiff = require('../mfDiff');
+
+const tests = [
+  ['C10', 'C5', 'C5'],
+  ['C10', 'ONC5', 'C5N-1O-1'],
+  ['C10', 'ONO', 'C10N-1O-2'],
+  ['OC10O', 'ONO', 'C10N-1'],
+  ['ClC10O', 'ONO', 'C10ClN-1O-1'],
+];
+
+describe('mfDiff', () => {
+  it.each(tests)('%s %s', (mf1, mf2, value) => {
+    expect(mfDiff(mf1, mf2)).toBe(value);
+  });
+});
---
+++ packages/mf-utilities/src/mfDiff.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const MF = require('mf-parser').MF;
+
+function mfDiff(mfString1, mfString2) {
+  let mf1 = new MF(mfString1).getInfo().atoms;
+  let mf2 = new MF(mfString2).getInfo().atoms;
+  let atoms = Object.keys(mf1);
+  Object.keys(mf2).map((atom) => {
+    if (!atoms.includes(atom)) atoms.push(atom);
+  });
+  let mf = '';
+  for (let atom of atoms) {
+    let diff = (mf1[atom] || 0) - (mf2[atom] || 0);
+    if (diff) mf += atom + diff;
+  }
+  return new MF(mf).toMF();
+}
+
+module.exports = mfDiff;
