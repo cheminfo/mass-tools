9366dcf8a95b240f7b75634fadb0b848d527fb17
Luc Patiny <luc@patiny.com>
2021-03-08 18:51:24 +0100

    chore: add testcase for charged ranges

--- packages/emdb/src/__tests__/fromRange.test.js
+++ packages/emdb/src/__tests__/fromRange.test.js
@@ -9,6 +9,12 @@ describe('fromRange', () => {
     expect(dbManager.databases.generated).toHaveLength(80);
   });
 
+  it('with charge', () => {
+    let dbManager = new DBManager();
+    dbManager.fromRange('C1-10 H1-10 (-)');
+    expect(dbManager.databases.generated[0].charge).toBe(-1);
+  });
+
   it('Filter callback', function () {
     let dbManager = new DBManager();
     dbManager.fromRange('C0-4 H0-4', {
