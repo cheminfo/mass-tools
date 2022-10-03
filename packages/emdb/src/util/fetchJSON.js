1f80541cf235f5b57d49929ff8242a45e5f45df4
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-03 17:36:44 +0200

    fix: test fetch with 300s timeout

--- packages/emdb/src/util/fetchJSON.js
+++ packages/emdb/src/util/fetchJSON.js
@@ -3,6 +3,16 @@
 const crossFetch = require('cross-fetch');
 
 module.exports = async function fetchJSONBrowser(url) {
-  const result = await crossFetch(url);
+  const controller = new AbortController();
+
+  // 5 second timeout:
+  const timeoutId = setTimeout(() => controller.abort(), 300000);
+  const result = await crossFetch(url, { signal: controller.signal }).then(
+    (response) => {
+      if (response) {
+        clearTimeout(timeoutId);
+      }
+    },
+  );
   return result.json();
 };
