f39bab354927285d2acdba741a80546c1113aed0
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-03 13:11:52 +0200

    fix: undo changes

--- packages/isotopic-distribution/src/IsotopicDistribution.js
+++ packages/isotopic-distribution/src/IsotopicDistribution.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const { ELECTRON_MASS } = require('chemical-elements');
-const { MF } = require('mf-parser');
+const ELECTRON_MASS = require('chemical-elements').ELECTRON_MASS;
+const MF = require('mf-parser').MF;
 const getMsInfo = require('mf-utilities/src/getMsInfo');
 const preprocessIonizations = require('mf-utilities/src/preprocessIonizations');
-const { SpectrumGenerator } = require('spectrum-generator');
+const SpectrumGenerator = require('spectrum-generator').SpectrumGenerator;
 
 const Distribution = require('./Distribution');
 const getDerivedCompositionInfo = require('./utils/getDerivedCompositionInfo');
