d82c0b1168f0f3c618a28dc0b8d89650afb7cccc
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-03 19:51:29 +0200

    fix: fetch timeout 50s

--- packages/emdb/src/util/fetchJSON.js
+++ packages/emdb/src/util/fetchJSON.js
@@ -3,6 +3,6 @@
 const crossFetch = require('cross-fetch');
 
 module.exports = async function fetchJSONBrowser(url) {
-  const result = await crossFetch(url);
+  const result = await crossFetch(url, { signal: AbortSignal.timeout(50000) });
   return result.json();
 };
