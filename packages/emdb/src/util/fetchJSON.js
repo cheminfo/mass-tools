c7d954b619d62b850aa95a033da8c28fc453f814
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-03 17:45:26 +0200

    fix: undo changes

--- packages/emdb/src/util/fetchJSON.js
+++ packages/emdb/src/util/fetchJSON.js
@@ -3,16 +3,6 @@
 const crossFetch = require('cross-fetch');
 
 module.exports = async function fetchJSONBrowser(url) {
-  const controller = new AbortController();
-
-  // 5 second timeout:
-  const timeoutId = setTimeout(() => controller.abort(), 300000);
-  const result = await crossFetch(url, { signal: controller.signal }).then(
-    (response) => {
-      if (response) {
-        clearTimeout(timeoutId);
-      }
-    },
-  );
+  const result = await crossFetch(url);
   return result.json();
 };
