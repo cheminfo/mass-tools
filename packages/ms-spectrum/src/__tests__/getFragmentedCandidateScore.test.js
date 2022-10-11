d0ac514052fb383fceeebf159b020a0d16703ada
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2022-10-11 10:48:33 +0200

    chore: generalized function

--- packages/ms-spectrum/src/__tests__/getFragmentedCandidateScore.test.js
+++ packages/ms-spectrum/src/__tests__/getFragmentedCandidateScore.test.js
@@ -3,39 +3,31 @@
 const getFragmentedCandidateScore = require('../getFragmentedCandidateScore.js');
 
 describe('test getMatchingScore', () => {
-  let matchedExpFragments = {
+  let experimentalSpecturm = {
     masses: [1, 2, 3, 4, 5, 6, 7, 8],
     intensities: [8, 7, 6, 5, 4, 3, 2, 1],
   };
-  let fragmentsContribution = [1, 2, 3, 4, 5];
-  let options = { massCoefficient: 2, intensityCoefficient: 0.8 };
+  let fragments = [1.00004, 2.00001, 3.00005, 4.00005, 5.000002];
+  let options = {
+    massCoefficient: 2,
+    intensityCoefficient: 0.8,
+    precision: 100,
+  };
 
   it('custom options', () => {
     let result = getFragmentedCandidateScore(
-      matchedExpFragments,
-      fragmentsContribution,
+      experimentalSpecturm,
+      fragments,
       options,
     );
-    expect(result).toBeCloseTo(446.76604692483096);
+    expect(result).toBeCloseTo(195.75600305170587);
   });
   it('default options', () => {
-    let result = getFragmentedCandidateScore(
-      matchedExpFragments,
-      fragmentsContribution,
-    );
-    expect(result).toBeCloseTo(2028.039654693095);
+    let result = getFragmentedCandidateScore(experimentalSpecturm, fragments);
+    expect(result).toBeCloseTo(287.1745887492587);
   });
-  it('missing fragmentsContribution and options', () => {
-    let result = getFragmentedCandidateScore(matchedExpFragments);
-    expect(result).toBeCloseTo(2013.039654693095);
-  });
-
-  it('all zeros', () => {
-    let matchedExpFragmentsAllZeros = {
-      masses: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      intensities: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    };
-    let result = getFragmentedCandidateScore(matchedExpFragmentsAllZeros);
+  it('missing fragments and options', () => {
+    let result = getFragmentedCandidateScore(experimentalSpecturm);
     expect(result).toBeCloseTo(0);
   });
 });
--- packages/ms-spectrum/src/getFragmentedCandidateScore.js
+++ packages/ms-spectrum/src/getFragmentedCandidateScore.js
@@ -1,41 +1,57 @@
 'use strict';
 
-const { xSum } = require('ml-spectra-processing');
-
 /**
- * @description Get the matching score of in-silico matched fragments by taking into account the mass and intensity of the matched fragments and the bond contribution of the matched fragments
- * @param {object} [matchedExpFragments={}] - object of all the experimental fragments matched
- * @param {Array} [matchedExpFragments.masses] - array of masses of the matched experimental fragments
- * @param {Array} [matchedExpFragments.intensities] - array of intensities of the matched experimental fragments
- * @param {Array} [fragmentsContribution=[]] - array of all the bond contributions for of matching fragments
- * @param {object} [options={}] - object containing the scale factor for mass and intensity
+ * @description A function who returns a matching score for a list of fragments
+ * @param {object} [experimentalSpectrum={}] - the experimental spectrum
+ * @param {Array} [experimentalSpectrum.masses] - array of masses of the experimental spectrum
+ * @param {Array} [experimentalSpectrum.intensities] - array of intensities of the experimental spectrum
+ * @param {Array} [fragmentResults=[]] - fragments to be matched
+ * @param {object} [options={}] - object containing the scale factor for mass and intensity and the precision in ppm
  * @param {number} [options.massCoefficient=3] - scale factor for mass
  * @param {number} [options.intensityCoefficient=0.6] - scale factor for intensity
- * @returns {number} - returns the matching score of the candidate molecule
+ * @param {number} [options.precision=5] - scale factor for intensity
+ * @returns {number} returns the matching score of the candidate molecule
  */
 
 // Scaling mass and intensity power can be found in DOI: 10.1016/1044-0305(94)87009-8
 
 function getFragmentedCandidateScore(
-  matchedExpFragments = { masses: [], intensities: [] },
-  fragmentsContribution = [0],
+  experimentalSpectrum = { masses: [], intensities: [] },
+  fragmentResults = [],
   options = {},
 ) {
-  const { massCoefficient = 3, intensityCoefficient = 0.6 } = options;
-  // sum all bonds contribution of matched fragments
-  let sumContribution = xSum(fragmentsContribution);
-
+  const {
+    massCoefficient = 3,
+    intensityCoefficient = 0.6,
+    precision = 5,
+  } = options;
+  let matchedExpFragments = { masses: [], intensities: [] };
+  for (let i = 0; i < experimentalSpectrum.masses.length; i++) {
+    let massExperimental = experimentalSpectrum.masses[i];
+    let massAccuracyOfFragment = (precision * massExperimental) / 1e6;
+    for (let j = 0; j < fragmentResults.length; j++) {
+      let massFragment = fragmentResults[j];
+      if (
+        // use absolute value to avoid negative values
+        Math.abs(massExperimental - massFragment) <= massAccuracyOfFragment
+      ) {
+        matchedExpFragments.masses.push(massExperimental);
+        matchedExpFragments.intensities.push(
+          experimentalSpectrum.intensities[i],
+        );
+        break;
+      }
+    }
+  }
   // Scale the mass and intensity
-  let weightFactor = 0;
+  let score = 0;
   for (let i = 0; i < matchedExpFragments.masses.length; i++) {
-    weightFactor +=
+    score +=
       matchedExpFragments.intensities[i] ** intensityCoefficient *
       matchedExpFragments.masses[i] ** massCoefficient;
   }
 
-  // final score
-  let finalScore = weightFactor + sumContribution;
-  return finalScore;
+  return score;
 }
 
 module.exports = getFragmentedCandidateScore;
