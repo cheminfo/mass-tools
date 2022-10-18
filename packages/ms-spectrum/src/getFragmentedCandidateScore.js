f8502abf8b98a700a91c39b0ca12aec5a0607f3e
Luc Patiny <luc@patiny.com>
2022-10-18 10:40:32 +0200

    chore: improve docs

--- packages/ms-spectrum/src/getFragmentedCandidateScore.js
+++ packages/ms-spectrum/src/getFragmentedCandidateScore.js
@@ -1,23 +1,22 @@
 'use strict';
 
 /**
- * @description A function who returns a matching score for a list of fragments
+ * @description A function which returns a matching score for a list of fragments
+ * Scaling mass and intensity power can be found in DOI: 10.1016/1044-0305(94)87009-8
  * @param {object} [experimentalSpectrum={}] - the experimental spectrum
- * @param {Array} [experimentalSpectrum.masses] - array of masses of the experimental spectrum
- * @param {Array} [experimentalSpectrum.intensities] - array of intensities of the experimental spectrum
- * @param {Array} [fragmentResults=[]] - fragments to be matched
+ * @param {number[]} [experimentalSpectrum.masses] - array of masses of the experimental spectrum
+ * @param {number[]} [experimentalSpectrum.intensities] - array of intensities of the experimental spectrum
+ * @param {number[]} [fragmentMasses=[]] - fragments to be matched
  * @param {object} [options={}] - object containing the scale factor for mass and intensity and the precision in ppm
  * @param {number} [options.massCoefficient=3] - scale factor for mass
  * @param {number} [options.intensityCoefficient=0.6] - scale factor for intensity
- * @param {number} [options.precision=5] - scale factor for intensity
+ * @param {number} [options.precision=5] - experimental precision in ppm
  * @returns {number} returns the matching score of the candidate molecule
  */
 
-// Scaling mass and intensity power can be found in DOI: 10.1016/1044-0305(94)87009-8
-
 function getFragmentedCandidateScore(
   experimentalSpectrum = { masses: [], intensities: [] },
-  fragmentResults = [],
+  fragmentMasses = [],
   options = {},
 ) {
   const {
@@ -28,18 +27,17 @@ function getFragmentedCandidateScore(
   let matchedExpFragments = { masses: [], intensities: [] };
   for (let i = 0; i < experimentalSpectrum.masses.length; i++) {
     let massExperimental = experimentalSpectrum.masses[i];
-    let massAccuracyOfFragment = (precision * massExperimental) / 1e6;
-    for (let j = 0; j < fragmentResults.length; j++) {
-      let massFragment = fragmentResults[j];
+    let allowedError = (precision * massExperimental) / 1e6;
+    for (let j = 0; j < fragmentMasses.length; j++) {
+      let massFragment = fragmentMasses[j];
       if (
         // use absolute value to avoid negative values
-        Math.abs(massExperimental - massFragment) <= massAccuracyOfFragment
+        Math.abs(massExperimental - massFragment) <= allowedError
       ) {
         matchedExpFragments.masses.push(massExperimental);
         matchedExpFragments.intensities.push(
           experimentalSpectrum.intensities[i],
         );
-        break;
       }
     }
   }
