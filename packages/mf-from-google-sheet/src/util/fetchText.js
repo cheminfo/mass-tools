b2ceec4a5826a5ec86af42631ed3680eb6efcc45
Luc Patiny <lpatiny@users.noreply.github.com>
2022-11-02 10:26:54 +0100

    fix(mf-from-google-sheet): remove request library

--- packages/mf-from-google-sheet/src/util/fetchText.js
+++ packages/mf-from-google-sheet/src/util/fetchText.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const request = require('request-promise-native');
+// eslint-disable-next-line no-shadow
+const fetch = require('cross-fetch');
 
-module.exports = function fetchText(url) {
-  return request(url).promise();
+module.exports = async function fetchText(url) {
+  const result = await fetch(url);
+  if (result.status !== 200) {
+    throw new Error(String(result.status));
+  }
+  return result.text();
 };
--- packages/mf-from-google-sheet/src/util/fetchTextBrowser.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/* eslint-env browser */
-
-module.exports = async function fetchTextBrowser(url) {
-  const result = await fetch(url);
-  return result.text();
-};
