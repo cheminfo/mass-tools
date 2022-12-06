7b6ded63bb6aa4d1cab7da8f5521a7cae6b5cc09
Luc Patiny <luc@patiny.com>
2022-12-06 14:57:10 +0100

    fix: create more compatible build of mass-tools

--- packages/mass-tools/src/index.js
+++ packages/mass-tools/src/index.js
@@ -1,19 +1,33 @@
+import * as EMDBPkg from 'emdb';
 import * as ReportPkg from 'ms-report';
+import * as SpectrumPkg from 'ms-spectrum';
 import * as NucleotidePkg from 'nucleotide';
 import * as PeptidePkg from 'peptide';
 
-export { EMDB } from 'emdb';
+export const Nucleotide = NucleotidePkg;
+export const Report = ReportPkg;
+export const Peptide = PeptidePkg;
+
 export { groups, groupsObject } from 'chemical-groups';
 export { elements, elementsObject } from 'chemical-elements';
 export { IsotopicDistribution } from 'isotopic-distribution';
-export { Spectrum, getPeaks, getBestPeaks } from 'ms-spectrum';
+
+EMDBPkg.EMDB.searchPubchem = EMDBPkg.searchPubchem;
+EMDBPkg.EMDB.searchActivesOrNaturals = EMDBPkg.searchActivesOrNaturals;
+EMDBPkg.EMDB.massShifts = EMDBPkg.massShifts;
+EMDBPkg.EMDB.prototype.searchPubchem = EMDBPkg.searchPubchem;
+EMDBPkg.EMDB.prototype.searchActivesOrNaturals =
+  EMDBPkg.searchActivesOrNaturals;
+EMDBPkg.EMDB.prototype.massShifts = EMDBPkg.massShifts;
+export const EMDB = EMDBPkg.EMDB;
+
+SpectrumPkg.Spectrum.JsGraph = SpectrumPkg.JsGraph;
+SpectrumPkg.Spectrum.prototype.JsGraph = SpectrumPkg.JsGraph;
+export const Spectrum = SpectrumPkg.Spectrum;
+
 export { MF } from 'mf-parser';
 export { preprocessRanges, getRangesForFragment } from 'mf-utilities';
 export { generateMFs } from 'mf-generator';
 
 export { atomSorter } from 'atom-sorter';
 export { mfFromEA } from 'mf-from-ea';
-
-export const Nucleotide = NucleotidePkg;
-export const Report = ReportPkg;
-export const Peptide = PeptidePkg;
