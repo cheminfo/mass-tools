1c532f3fb01e9a42db619d4fdde9334a4538b066
Luc Patiny <luc@patiny.com>
2022-12-16 08:55:20 +0100

    feat(mf-finder): Add docs and filter options to mfIncluded

--- packages/mf-finder/src/__tests__/mfIncluded.test.js
+++ packages/mf-finder/src/__tests__/mfIncluded.test.js
@@ -8,5 +8,20 @@ test('mfIncluded', () => {
   expect(mfIncluded('CH3', 'Me0-5')).toBeTruthy();
   expect(mfIncluded('C2H6', 'Me0-5')).toBeTruthy();
   expect(mfIncluded('C2H6O', 'Me0-5(CH3O)0-5')).toBeTruthy();
+  expect(
+    mfIncluded('C2H6O', 'Me0-5(CH3O)0-5', {
+      filter: { unsaturation: { min: 0 } },
+    }),
+  ).toBeTruthy();
+  expect(
+    mfIncluded('C2H6O', 'Me0-5(CH3O)0-5', {
+      filter: { unsaturation: { min: 1 } },
+    }),
+  ).toBeFalsy();
+  expect(
+    mfIncluded('C2H6O', 'Me0-5(CH3O)0-5', {
+      filter: { unsaturation: { min: 1 } },
+    }),
+  ).toBeFalsy();
   expect(mfIncluded('C2H6O', 'Me0-5')).toBeFalsy();
 });
--- packages/mf-finder/src/mfIncluded.js
+++ packages/mf-finder/src/mfIncluded.js
@@ -2,13 +2,35 @@ import { MF } from 'mf-parser';
 
 import { findMFsSync } from './findMFsSync.js';
 
-export function mfIncluded(target, range) {
-  let targetEM = new MF(target).getInfo().monoisotopicMass;
+/**
+ * @param {string}        mf - the molecular formula to check
+ * @param {string}        range - the range of MF to explore
+ * @param {object}        [options={}]
+ * @param {number}        [options.maxIterations=10000000] - Maximum number of iterations
+ * @param {boolean}       [options.allowNeutral=true]
+ * @param {string}        [options.ionizations=''] - string containing a comma separated list of modifications
+ * @param {object}        [options.filter={}]
+ * @param {number}        [options.filter.minCharge=-Infinity] - Minimal charge
+ * @param {number}        [options.filter.maxCharge=+Infinity] - Maximal charge
+ * @param {object}        [options.filter.unsaturation={}]
+ * @param {number}        [options.filter.unsaturation.min=-Infinity] - Minimal unsaturation
+ * @param {number}        [options.filter.unsaturation.max=+Infinity] - Maximal unsaturation
+ * @param {number}        [options.filter.unsaturation.onlyInteger=false] - Integer unsaturation
+ * @param {number}        [options.filter.unsaturation.onlyNonInteger=false] - Non integer unsaturation
+ * @param {object}        [options.filter.atoms] - object of atom:{min, max}
+ * @param {function}      [options.filter.callback] - a function to filter the MF
+ * @returns {boolean}
+ */
+export function mfIncluded(mf, range, options = {}) {
+  let targetEM = new MF(mf).getInfo().monoisotopicMass;
   let results = findMFsSync(targetEM, {
-    ranges: range,
-    allowNeutral: true,
-    precision: 0.0000001,
-    limit: 1,
+    ...{ allowNeutral: true },
+    ...options,
+    ...{
+      ranges: range,
+      precision: 0.0000001,
+      limit: 1,
+    },
   });
   return results.mfs.length > 0;
 }
