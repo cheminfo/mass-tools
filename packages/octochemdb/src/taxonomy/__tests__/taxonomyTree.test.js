ae76de5e483f333c773fdf5768d74beabf09829d
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-06-13 14:08:27 +0200

    chore: update test case

--- packages/octochemdb/src/taxonomy/__tests__/taxonomyTree.test.js
+++ packages/octochemdb/src/taxonomy/__tests__/taxonomyTree.test.js
@@ -44,7 +44,165 @@ test('taxonomyTree', () => {
   ];
 
   let tree = taxonomyTree(taxonomies);
-  expect(tree[0].children).toHaveLength(2);
-  expect(tree[0].children[1].name).toBe('Fungi');
-  expect(tree[0].children[0].name).toBe('Apicomplexa');
+  expect(tree[1].children).toHaveLength(2);
+  expect(tree[1].children[0].name).toBe('Apicomplexa');
+  expect(tree[1].children[1].name).toBe('Fungi');
+  expect(JSON.stringify(tree, null, 2)).toMatchInlineSnapshot(`
+    "[
+      {
+        \\"name\\": \\"Eukaryota\\",
+        \\"rank\\": \\"superKingdom\\",
+        \\"children\\": [
+          {
+            \\"name\\": \\"Fungi\\",
+            \\"rank\\": \\"kingdom\\",
+            \\"children\\": [
+              {
+                \\"name\\": \\"Ascomycota\\",
+                \\"rank\\": \\"phylum\\",
+                \\"children\\": [
+                  {
+                    \\"name\\": \\"Saccharomycetes\\",
+                    \\"rank\\": \\"class\\",
+                    \\"children\\": [
+                      {
+                        \\"name\\": \\"Saccharomycetales\\",
+                        \\"rank\\": \\"order\\",
+                        \\"children\\": [
+                          {
+                            \\"name\\": \\"Debaryomycetaceae\\",
+                            \\"rank\\": \\"family\\",
+                            \\"children\\": [
+                              {
+                                \\"name\\": \\"Candida\\",
+                                \\"rank\\": \\"genus\\",
+                                \\"children\\": [
+                                  {
+                                    \\"name\\": \\"Candida albicans\\",
+                                    \\"rank\\": \\"species\\"
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    \\"name\\": \\"Sordariomycetes\\",
+                    \\"rank\\": \\"class\\",
+                    \\"children\\": [
+                      {
+                        \\"name\\": \\"Hypocreales\\",
+                        \\"rank\\": \\"order\\",
+                        \\"children\\": [
+                          {
+                            \\"name\\": \\"Stachybotryaceae\\",
+                            \\"rank\\": \\"family\\",
+                            \\"children\\": [
+                              {
+                                \\"name\\": \\"Stachybotrys\\",
+                                \\"rank\\": \\"genus\\",
+                                \\"children\\": [
+                                  {
+                                    \\"name\\": \\"Stachybotrys chartarum\\",
+                                    \\"rank\\": \\"species\\"
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        \\"name\\": \\"bacteria\\",
+        \\"rank\\": \\"superKingdom\\",
+        \\"children\\": [
+          {
+            \\"name\\": \\"Apicomplexa\\",
+            \\"rank\\": \\"phylum\\",
+            \\"children\\": [
+              {
+                \\"name\\": \\"Aconoidasida\\",
+                \\"rank\\": \\"class\\",
+                \\"children\\": [
+                  {
+                    \\"name\\": \\"Haemosporida\\",
+                    \\"rank\\": \\"order\\",
+                    \\"children\\": [
+                      {
+                        \\"name\\": \\"Plasmodiidae\\",
+                        \\"rank\\": \\"family\\",
+                        \\"children\\": [
+                          {
+                            \\"name\\": \\"Plasmodium\\",
+                            \\"rank\\": \\"genus\\",
+                            \\"children\\": [
+                              {
+                                \\"name\\": \\"Plasmodium falciparum\\",
+                                \\"rank\\": \\"species\\"
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            \\"name\\": \\"Fungi\\",
+            \\"rank\\": \\"kingdom\\",
+            \\"children\\": [
+              {
+                \\"name\\": \\"Ascomycota\\",
+                \\"rank\\": \\"phylum\\",
+                \\"children\\": [
+                  {
+                    \\"name\\": \\"Sordariomycetes\\",
+                    \\"rank\\": \\"class\\",
+                    \\"children\\": [
+                      {
+                        \\"name\\": \\"Hypocreales\\",
+                        \\"rank\\": \\"order\\",
+                        \\"children\\": [
+                          {
+                            \\"name\\": \\"Stachybotryaceae\\",
+                            \\"rank\\": \\"family\\",
+                            \\"children\\": [
+                              {
+                                \\"name\\": \\"Stachybotrys\\",
+                                \\"rank\\": \\"genus\\",
+                                \\"children\\": [
+                                  {
+                                    \\"name\\": \\"Stachybotrys cylindrospora\\",
+                                    \\"rank\\": \\"species\\"
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]"
+  `);
 });
