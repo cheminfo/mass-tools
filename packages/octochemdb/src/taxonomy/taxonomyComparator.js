d3728919a910bb2e78f8fcd7f9c4f55119de57f2
Luc Patiny <luc@patiny.com>
2023-06-13 09:33:46 +0200

    chore(octochemdb): refactor taxonomy

--- packages/octochemdb/src/taxonomy/taxonomyComparator.js
+++ packages/octochemdb/src/taxonomy/taxonomyComparator.js
@@ -1,35 +1,12 @@
-/**
- * An object containing two arrays
- * @typedef {object} Taxonomy
- * @property {string} [superKingdom] - The superKingdom
- * @property {string} [kingdom] - The kingdom
- * @property {string} [phylum] - The phylum
- * @property {string} [class] - The class
- * @property {string} [order] - The order
- * @property {string} [family] - The family
- * @property {string} [genus] - The genus
- * @property {string} [species] - The species
- */
-
-const fields = [
-  'superKingdom',
-  'kingdom',
-  'phylum',
-  'class',
-  'order',
-  'family',
-  'genus',
-  'species',
-];
-
+import { taxonomyRanks } from './taxonomyRanks.js';
 /**
  * @description sort taxonomies by superKingdom, kingdom, phylum, class, order, family, genus, species
- * @param {Taxonomy} a
- * @param {Taxonomy} b
+ * @param {import('./Taxonomy.js').Taxonomy} a
+ * @param {import('./Taxonomy.js').Taxonomy} b
  * @returns {number}
  */
 export function taxonomyComparator(a, b) {
-  for (const field of fields) {
+  for (const field of taxonomyRanks) {
     if (a[field] !== b[field]) {
       return (a[field] || '').localeCompare(b[field] || '');
     }
---
+++ packages/octochemdb/src/taxonomy/taxonomyRanks.js
@@ -0,0 +1,10 @@
+export const taxonomyRanks = [
+  'superKingdom',
+  'kingdom',
+  'phylum',
+  'class',
+  'order',
+  'family',
+  'genus',
+  'species',
+];
