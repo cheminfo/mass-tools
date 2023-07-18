02e9ff8cf3b1b45562e29464abe961c966013909
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-07-18 12:15:20 +0200

    chore: generalize databases

--- packages/mass-fragmentation/src/reactionFragmentation.js
+++ packages/mass-fragmentation/src/reactionFragmentation.js
@@ -1,16 +1,13 @@
 import { applyReactions } from 'openchemlib-utils';
 
-import { cid } from './database/collisionInducedDissociation';
+import getDatabase from './database/getDatabase';
 import { insertMfInfoFragments } from './utils/insertMfInfoFragments';
 
-const databases = {
-  cid,
-};
 /**
  * @description Fragment a molecule by applying reactions from a custom database of reactions
  * @param {import('openchemlib').Molecule} molecule - The OCL molecule to be fragmented
  * @param {Object}  [options={}]
- * @param {string}  [options.database='cid'] - The database to be used
+ * @param {string}  [options.databaseName='cid'] - The database to be used
  * @param {string}  [options.mode='positive'] - The mode to be used
  * @param {number}  [options.maxDepth=5] - The maximum depth of the fragmentation tree
  * @returns {object} In-Silico fragmentation results with the following properties:
@@ -19,9 +16,13 @@ const databases = {
  * - products: array of trees grouped by product idCode
  */
 export function reactionFragmentation(molecule, options = {}) {
-  let { database = 'cid', mode = 'positive', maxDepth = 5 } = options;
+  let { databaseName = 'cid', mode = 'positive', maxDepth = 5 } = options;
+  let database = getDatabase(databaseName);
+  if (!database) {
+    throw new Error(`Database ${databaseName} not found`);
+  }
 
-  const reactions = databases[database][mode];
+  const reactions = database[mode];
   let fragments = applyReactions([molecule], reactions, {
     maxDepth,
   });
