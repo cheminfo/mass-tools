8489705bad049383dd7fa22fe79cdb4f99edb709
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-08-25 15:06:33 +0200

    fix(octochemdb): retry if fetch fail

--- packages/octochemdb/src/utils/fetchJSON.js
+++ packages/octochemdb/src/utils/fetchJSON.js
@@ -1,8 +1,14 @@
 export async function fetchJSON(url, data) {
   if (data) {
+    try {
     const searchParams = new URLSearchParams(data);
     const result = await fetch(`${url}?${searchParams.toString()}`);
     return result.json();
+    } catch (e) {
+      const searchParams = new URLSearchParams(data);
+      const result = await fetch(`${url}?${searchParams.toString()}`);
+      return result.json();
+    }
   } else {
     const result = await fetch(url);
     return result.json();
