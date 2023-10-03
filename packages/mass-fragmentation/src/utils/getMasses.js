8a5d4cdfaf5662e38fb81baf85ab82e380a33bc8
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-03 13:32:57 +0200

    chore: get masses from new tree structure

--- packages/mass-fragmentation/src/utils/getMasses.js
+++ packages/mass-fragmentation/src/utils/getMasses.js
@@ -1,33 +1,26 @@
 /**
  * @description get array of mz from fragmentation trees
  * @param {Array} trees Fragmentation trees
- * @param {Array} products Fragmentation trees grouped by product idCode
- * @returns {object} Object with the following properties:
- * - masses: array of monoisotopic masses
- * - trees: array of fragmentation trees
- * - products: array of trees grouped by product idCode
+ * @returns {Array}  array of mz values
+
  */
-export function getMasses(trees, products) {
+export function getMasses(trees) {
   let masses = {};
   for (const tree of trees) {
     getMassesFromTree(tree, masses);
   }
-  return {
-    masses: Object.keys(masses).map(Number),
-    trees,
-    products,
-  };
+  return Object.keys(masses).map(Number);
 }
 
 function getMassesFromTree(currentBranch, masses) {
-  for (const product of currentBranch.products) {
-    if (Math.abs(product.charge) > 0) {
-      masses[product.mz] = true;
+  for (const molecule of currentBranch.molecules) {
+    if (Math.abs(molecule.info.charge) > 0) {
+      masses[molecule.info.mz] = true;
     }
-    if (product.children.length > 0) {
-      for (const child of product.children) {
-        getMassesFromTree(child, masses);
-      }
+  }
+  if (currentBranch.children && currentBranch.children.length > 0) {
+    for (const child of currentBranch.children) {
+      getMassesFromTree(child, masses);
     }
   }
 }
