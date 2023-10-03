67668f4810a4acfe086e1ff1ddf363505794b21c
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-03 13:51:50 +0200

    chore: rm .only from test

--- packages/mass-fragmentation/src/__tests__/reactionFragmentation.test.js
+++ packages/mass-fragmentation/src/__tests__/reactionFragmentation.test.js
@@ -5,7 +5,7 @@ import { reactionFragmentation } from '../reactionFragmentation.js';
 const { Molecule } = OCL;
 
 describe('ReactionFragmentation', async () => {
-  it.only('Alpha cleavage: MDMA', async () => {
+  it('Alpha cleavage: MDMA', async () => {
     const molecule = Molecule.fromSmiles('CNC(Cc1ccc2c(c1)OCO2)C');
     const { trees, validNodes, masses } = reactionFragmentation(molecule);
     expect(validNodes).toHaveLength(274);
