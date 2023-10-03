6b2b32944a0c29c281ecfe8267e0e21834c8d692
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-03 13:34:09 +0200

    feat: reactionFragmentation using Reaction class

--- packages/mass-fragmentation/src/reactionFragmentation.js
+++ packages/mass-fragmentation/src/reactionFragmentation.js
@@ -6,38 +6,36 @@ import { getDatabases } from './database/getDatabases';
 import { getMasses } from './utils/getMasses';
 /**
  * @description Fragment a molecule by applying reactions from a custom database of reactions
- * @param {import('openchemlib').Molecule} molecule - The OCL molecule to be fragmented
+ * @param {import('openchemlib').Molecule} oclMolecule - The OCL molecule to be fragmented
  * @param {Object}  [options={}]
- * @param {string}  [options.databaseName='cid'] - The database to be used
  * @param {string}  [options.mode='positive'] - The mode to be used
- * @param {number}  [options.maxDepth=5] - The maximum depth of the fragmentation tree
- * @param {boolean}  [options.getProductsTrees=false] - If true, the products trees are returned else products is an empty array
+ * @param {number}  [options.maxDepth=5] - The maximum depth of the overall fragmentation tree
  * @param {number}  [options.limitReactions=200] - The maximum number of reactions to be applied
+ * @param {string}  [options.dwarEntry] - The dwar entry to be used, if not provided, the default one will be used
  * @param {number}  [options.maxIonizationDepth=1] - The maximum depth of the ionization tree
- * @param {Object}  [options.customDatabase={}] - A custom database of reactions
- * @param {Array}  [options.customDatabase.positive] - A custom database of reactions for positive mode
- * @param {Array}  [options.customDatabase.negative] - A custom database of reactions for negative mode
- * @param {Object}  [options.customDatabase.ionization] - A custom database ionization reactions
- * @param {Array}  [options.customDatabase.ionization.positive] - A custom database of ionization reactions for positive mode
- * @param {Array}  [options.customDatabase.ionization.negative] - A custom database of ionization reactions for negative mode
+ * @param {number}  [options.minIonizationDepth=1] - The minimum depth of the ionization tree
+ * @param {number}  [options.minReactionDepth=0] - The minimum depth of the reaction tree
+ * @param {number}  [options.maxReactionDepth=3] - The maximum depth of the reaction tree
  * @returns {object} In-Silico fragmentation results with the following properties:
  * - masses: array of monoisotopic masses
  * - trees: array of fragmentation trees
- * - products: array of trees grouped by product idCode
+ * - validNodes: nodes without dead branches
  */
-export function reactionFragmentation(molecule, options = {}) {
+export function reactionFragmentation(oclMolecule, options = {}) {
   const {
-    ionization = 'cid',
     mode = 'positive',
+    dwarEntry = '',
     maxDepth = 5,
-    maxIonizationDepth = 1,
-    getProductsTrees = false,
-    databases = getDatabases(),
     limitReactions = 200,
+    minIonizationDepth = 1,
+    maxIonizationDepth = 1,
+    minReactionDepth = 0,
+    maxReactionDepth = 3,
   } = options;
 
   const reactions = new Reactions(OCL, {
     moleculeInfoCallback: (molecule) => {
+      // @ts-ignore
       const mf = getMF(molecule).mf;
       const mfInfo = new MF(mf).getInfo();
       return {
@@ -49,24 +47,33 @@ export function reactionFragmentation(molecule, options = {}) {
       };
     },
     maxDepth,
+    limitReactions,
     skipProcessed: true,
   });
 
-  reactions.appendHead([molecule]);
-  reactions.applyOneReactantReactions(databases.ionizations, {
-    min: 1,
-    max: 1,
-  });
-  reactions.applyOneReactantReactions(databases.fragmentations, {
-    min: 0,
-    max: 2,
-  });
+  reactions.appendHead([oclMolecule]);
+  reactions.applyOneReactantReactions(
+    getDatabases({ kind: 'ionization', mode }, dwarEntry),
+    {
+      min: minIonizationDepth,
+      max: maxIonizationDepth,
+    },
+  );
+  reactions.applyOneReactantReactions(
+    getDatabases({ kind: 'reaction', mode }, dwarEntry),
+    {
+      min: minReactionDepth,
+      max: maxReactionDepth,
+    },
+  );
 
   const trees = reactions.trees;
   const validNodes = reactions.getValidNodes();
+  const masses = getMasses(trees);
 
   return {
     trees,
     validNodes,
+    masses,
   };
 }
