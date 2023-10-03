0ee376dab4be4a7fe127662b8981c4463d0de0cf
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-03 13:32:42 +0200

    feat: get DB from dwar entry

--- packages/mass-fragmentation/src/database/getDatabases.js
+++ packages/mass-fragmentation/src/database/getDatabases.js
@@ -1,17 +1,30 @@
-import { defaultDatabase } from './database.js';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
-export function getDatabases(options = {}) {
-  const {
-    ionization = 'esi',
-    mode = 'positive',
-    database = defaultDatabase,
-  } = options;
+import { parseDwar } from 'openchemlib-utils';
+/**
+ * @description Get the default databases of reactions for positive and negative mode
+ * @param {Object} options - Options for database selection
+ * @param {string} [options.mode='positive'] - The mode to be used
+ * @param {string} [options.kind='ionization'] - The kind of database to be used
+ * @param {string} dwarEntry - The dwar entry to be used
+ * @returns
+ */
+export function getDatabases(options = {}, dwarEntry = '') {
+  const { mode = 'positive', kind = 'ionization' } = options;
+  if (!dwarEntry) {
+    dwarEntry = readFileSync(
+      join(__dirname, './ReactionMassFragmentation.dwar'),
+      'utf8',
+    );
+  }
 
-  // need to filter database by ionization and mode somehow
-  const ionizations = database.filter((entry) => entry.kind === 'ionization');
-  const fragmentations = database.filter(
-    (entry) => entry.kind !== 'ionization',
+  const data = parseDwar(dwarEntry).data;
+  const database = data.filter(
+    (entry) =>
+      // @ts-ignore
+      entry.kind === kind && (entry.mode === mode || entry?.mode === 'both'),
   );
 
-  return { ionizations, fragmentations };
+  return database;
 }
