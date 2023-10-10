6f961b5aa96e4bbfc32beafe12a1f50b5e07a97d
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-10 13:48:44 +0200

    chore(mass-fragmentation): export getDatabase

--- packages/mass-fragmentation/src/index.js
+++ packages/mass-fragmentation/src/index.js
@@ -1,4 +1,4 @@
 export * from './fragment.js';
 export * from './reactionFragmentation.js';
 export * from './getFragmentationSVG.js';
-export * from './database/getDatabases.js';
+export * from './database/getDatabase.js';
