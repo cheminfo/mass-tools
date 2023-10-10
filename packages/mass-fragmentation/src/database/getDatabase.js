47fff8dd8b56f7ed038470ce0d7306d901f0d82d
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-10-10 13:38:17 +0200

    chore(mass-fragmentation): return all db by default

--- packages/mass-fragmentation/src/database/getDatabase.js
+++ packages/mass-fragmentation/src/database/getDatabase.js
@@ -4,28 +4,22 @@ import { defaultDatabase } from './defaultDatabase.js';
 /**
  * @description Get the default databases of reactions for positive and negative mode
  * @param {Object} options - Options for database selection
- * @param {'positive'|'negative'|'both'} [options.mode='both'] - The mode to be used
- * @param {'ionization'|'resonance'|'reaction'} [options.kind='ionization'] - The kind of database to be used
- * @param {'esi'|'ei'} [options.ionizationKind='esi'] - The ionization technique to be used
+ * @param {'ionization'|'resonance'|'reaction'|'all'} [options.kind='all'] - The kind of database to be used
+ * @param {'esiPos'|'esiNeg'|'ei'|'all'} [options.ionizationKind='all'] - The ionization technique to be used
  * @param {string} [options.dwar] - The dwar file to use. Default will use an included mass fragmentation database
  * @returns
  */
 export function getDatabase(options = {}) {
-  const {
-    mode = 'both',
-    kind = 'ionization',
-    ionizationKind = 'esi',
-    dwar,
-  } = options;
+  const { kind = 'all', ionizationKind = 'all', dwar } = options;
 
   const fullDatabase = dwar ? parseDwar(dwar).data : defaultDatabase;
 
   const database = fullDatabase.filter(
     (entry) =>
-      entry.kind === kind &&
-      (entry.mode === mode || entry?.mode === 'both') &&
-      entry.ionization === ionizationKind,
+      (entry.ionization === ionizationKind && entry.kind === kind) ||
+      (kind === 'all' && entry.ionization === ionizationKind) ||
+      (entry.kind === kind && ionizationKind === 'all') ||
+      (kind === 'all' && ionizationKind === 'all'),
   );
-
   return database;
 }
