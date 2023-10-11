e3cd1b70ac4989b71fb7e0934853ba5c7b1cef91
Luc Patiny <luc@patiny.com>
2023-10-11 16:54:32 +0200

    feat(mass-fragmentation): add molecules with same mass

--- packages/mass-fragmentation/src/utils/getMasses.js
+++ packages/mass-fragmentation/src/utils/getMasses.js
@@ -9,9 +9,15 @@ export function getMasses(nodes) {
   for (const node of nodes) {
     for (let molecule of node.molecules) {
       if (molecule.info.mz) {
-        masses[molecule.info.mz] = true;
+        if (!masses[molecule.info.mz]) {
+          masses[molecule.info.mz] = { ...molecule.info, molecules: [], minDepth: node.depth };
+        }
+        masses[molecule.info.mz].molecules.push({ idCode: molecule.idCode, molfile: molecule.molfile, depth: node.depth })
+        if (masses[molecule.info.mz].minDepth > node.depth) {
+          masses[molecule.info.mz].minDepth = node.depth;
+        }
       }
     }
   }
-  return Object.keys(masses).map(Number);
+  return Object.values(masses);
 }
