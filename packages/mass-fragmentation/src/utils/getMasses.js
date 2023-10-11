eefc3cdf41e74caf07e1df5393b464bb6131affa
Luc Patiny <luc@patiny.com>
2023-10-11 16:57:16 +0200

    chore: fix prettier

--- packages/mass-fragmentation/src/utils/getMasses.js
+++ packages/mass-fragmentation/src/utils/getMasses.js
@@ -10,9 +10,17 @@ export function getMasses(nodes) {
     for (let molecule of node.molecules) {
       if (molecule.info.mz) {
         if (!masses[molecule.info.mz]) {
-          masses[molecule.info.mz] = { ...molecule.info, molecules: [], minDepth: node.depth };
+          masses[molecule.info.mz] = {
+            ...molecule.info,
+            molecules: [],
+            minDepth: node.depth,
+          };
         }
-        masses[molecule.info.mz].molecules.push({ idCode: molecule.idCode, molfile: molecule.molfile, depth: node.depth })
+        masses[molecule.info.mz].molecules.push({
+          idCode: molecule.idCode,
+          molfile: molecule.molfile,
+          depth: node.depth,
+        });
         if (masses[molecule.info.mz].minDepth > node.depth) {
           masses[molecule.info.mz].minDepth = node.depth;
         }
