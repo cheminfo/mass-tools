4c28dda0f9a4ad890e5da222342dca327975ad01
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2024-12-04 16:50:44 +0100

    chore: lint

--- packages/mf-parser/src/util/getInfo.types.ts
+++ packages/mf-parser/src/util/getInfo.types.ts
@@ -1,8 +1,10 @@
 import type { AtomsMap } from './partToAtoms';
 
-type CustomNameFields<Options extends GetInfoOptions<string, string>> =
-  Record<Exclude<Options['emFieldName'], undefined>, number>
-  & Record<Exclude<Options['msemFieldName'], undefined>, number>;
+type CustomNameFields<Options extends GetInfoOptions<string, string>> = Record<
+  Exclude<Options['emFieldName'], undefined>,
+  number
+> &
+  Record<Exclude<Options['msemFieldName'], undefined>, number>;
 
 export type PartInfo<Options extends GetInfoOptions<string, string>> = {
   mass: number;
@@ -12,18 +14,19 @@ export type PartInfo<Options extends GetInfoOptions<string, string>> = {
   unsaturation?: number;
 } & CustomNameFields<Options>;
 
-export type PartInfoWithParts<Options extends GetInfoOptions<string, string>> = {
-  parts: Array<PartInfo<Options>>;
-  mass: number;
-  charge: number;
-  unsaturation: number;
-  atoms: AtomsMap;
-  mf: string;
-} & CustomNameFields<Options>;
+export type PartInfoWithParts<Options extends GetInfoOptions<string, string>> =
+  {
+    parts: Array<PartInfo<Options>>;
+    mass: number;
+    charge: number;
+    unsaturation: number;
+    atoms: AtomsMap;
+    mf: string;
+  } & CustomNameFields<Options>;
 
 export interface GetInfoOptions<
   EM extends string = 'monoisotopicMass',
-  MSEM extends string = 'observedMonoisotopicMass'
+  MSEM extends string = 'observedMonoisotopicMass',
 > {
   customUnsaturations?: object;
   emFieldName?: EM;
