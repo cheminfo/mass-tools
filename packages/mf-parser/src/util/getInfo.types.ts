61a12c2f2d1ddc462c4bd58aab5ad730505e6a90
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2024-12-04 16:38:10 +0100

    docs(mf-parser): add customizable fields into PartInfo

--- packages/mf-parser/src/util/getInfo.types.ts
+++ packages/mf-parser/src/util/getInfo.types.ts
@@ -1,18 +1,31 @@
 import type { AtomsMap } from './partToAtoms';
 
-export interface PartInfo {
+type CustomNameFields<Options extends GetInfoOptions<string, string>> =
+  Record<Exclude<Options['emFieldName'], undefined>, number>
+  & Record<Exclude<Options['msemFieldName'], undefined>, number>;
+
+export type PartInfo<Options extends GetInfoOptions<string, string>> = {
   mass: number;
   charge: number;
   mf: string;
   atoms: AtomsMap;
   unsaturation?: number;
-}
+} & CustomNameFields<Options>;
 
-export interface PartInfoWithParts {
-  parts: PartInfo[];
+export type PartInfoWithParts<Options extends GetInfoOptions<string, string>> = {
+  parts: Array<PartInfo<Options>>;
   mass: number;
   charge: number;
   unsaturation: number;
   atoms: AtomsMap;
   mf: string;
+} & CustomNameFields<Options>;
+
+export interface GetInfoOptions<
+  EM extends string = 'monoisotopicMass',
+  MSEM extends string = 'observedMonoisotopicMass'
+> {
+  customUnsaturations?: object;
+  emFieldName?: EM;
+  msemFieldName?: MSEM;
 }
