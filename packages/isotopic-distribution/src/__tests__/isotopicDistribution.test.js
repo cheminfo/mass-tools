3cd50486a99cef7b2066ee37d13e215ab6db8bb9
Luc Patiny <lpatiny@users.noreply.github.com>
2024-12-05 15:59:43 +0100

    test(isotopic-distribution): add C1000000 test (#233)

--- packages/isotopic-distribution/src/__tests__/isotopicDistribution.test.js
+++ packages/isotopic-distribution/src/__tests__/isotopicDistribution.test.js
@@ -7,7 +7,7 @@ expect.extend({ toBeDeepCloseTo });
 
 describe('test isotopicDistribution', () => {
   it('create distribution of CH0', () => {
-    let isotopicDistribution = new IsotopicDistribution('CH00');
+    const isotopicDistribution = new IsotopicDistribution('CH00');
     expect(isotopicDistribution.getTable()).toStrictEqual([
       { x: 12, y: 0.9893 },
       { x: 13.00335483507, y: 0.0107 },
@@ -36,7 +36,7 @@ describe('test isotopicDistribution', () => {
       { x: 12, y: 100 },
       { x: 13.00335483507, y: 1.0815728292732234 },
     ]);
-    let distribution = isotopicDistribution.getDistribution();
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array[0].x).toBe(12);
   });
 
@@ -57,10 +57,10 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of C Ag+', () => {
-    let isotopicDistribution = new IsotopicDistribution('C', {
+    const isotopicDistribution = new IsotopicDistribution('C', {
       ionizations: 'Ag+',
     });
-    let distribution = isotopicDistribution.getDistribution();
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toStrictEqual([
       { x: 118.90454302009094, y: 0.512843227 },
       { x: 119.90789785516094, y: 0.005546773 },
@@ -70,8 +70,8 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution for multiplepart, C.C.C2', () => {
-    let isotopicDistribution = new IsotopicDistribution('C.C.C2');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C.C.C2');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([
       { x: 12, y: 1.9786 },
       { x: 13.00335483507, y: 0.0214 },
@@ -82,19 +82,19 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution with minY', () => {
-    let isotopicDistribution = new IsotopicDistribution('C', { minY: 0.1 });
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C', { minY: 0.1 });
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([{ x: 12, y: 0.9893 }]);
   });
 
   it('create distribution with impossible minY', () => {
-    let isotopicDistribution = new IsotopicDistribution('C', { minY: 1 });
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C', { minY: 1 });
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([]);
   });
 
   it('create distribution from parts', () => {
-    let parts = [
+    const parts = [
       {
         mf: 'C',
         intensity: 2,
@@ -107,8 +107,8 @@ describe('test isotopicDistribution', () => {
         ionization: { mf: '', em: 0, charge: 0 },
       },
     ];
-    let isotopicDistribution = new IsotopicDistribution(parts);
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution(parts);
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([
       { x: 12, y: 1.9786 },
       { x: 13.00335483507, y: 0.0214 },
@@ -119,26 +119,26 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of CN default res', () => {
-    let isotopicDistribution = new IsotopicDistribution('CN');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('CN');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution).toHaveLength(3);
   });
 
   it('create distribution of CN high res', () => {
-    let isotopicDistribution = new IsotopicDistribution('CN', { fwhm: 0 });
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('CN', { fwhm: 0 });
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution).toHaveLength(4);
   });
 
   it('create distribution of C1000', () => {
-    let isotopicDistribution = new IsotopicDistribution('C1000');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C1000');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array[0].x).toBe(12000);
   });
 
   it('create distribution for multiplepart, C.C2', () => {
-    let isotopicDistribution = new IsotopicDistribution('C.C2');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C.C2');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([
       { x: 12, y: 0.9893 },
       { x: 13.00335483507, y: 0.0107 },
@@ -148,14 +148,14 @@ describe('test isotopicDistribution', () => {
     ]);
   });
   it('create distribution for multiplepart, C.C2.C3', () => {
-    let isotopicDistribution = new IsotopicDistribution('C.C2.C3');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C.C2.C3');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array.reduce((e, p) => e + p.y, 0)).toBeCloseTo(3, 5);
   });
 
   it('create distribution for charged multiplepart, C+.(C+)2', () => {
-    let isotopicDistribution = new IsotopicDistribution('C+.(C+)2');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C+.(C+)2');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([
       { x: 11.99945142009093, y: 1.9680144899999998 },
       { x: 12.501128837625929, y: 0.02117102 },
@@ -164,18 +164,18 @@ describe('test isotopicDistribution', () => {
   });
 
   it('getParts of isotopic distribution', () => {
-    let isotopicDistribution = new IsotopicDistribution('C', {
+    const isotopicDistribution = new IsotopicDistribution('C', {
       ionizations: 'H+',
     });
-    let parts = isotopicDistribution.getParts();
+    const parts = isotopicDistribution.getParts();
     expect(parts[0].ms.em).toBeCloseTo(13.00727645232093, 5);
   });
 
   it('create distribution for many ionizations, C + (+, ++)', () => {
-    let isotopicDistribution = new IsotopicDistribution('C', {
+    const isotopicDistribution = new IsotopicDistribution('C', {
       ionizations: '+,++',
     });
-    let distribution = isotopicDistribution.getDistribution();
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([
       { x: 5.99945142009093, y: 0.9893 },
       { x: 6.50112883762593, y: 0.0107 },
@@ -186,10 +186,10 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution for many ionizations, C + H+', () => {
-    let isotopicDistribution = new IsotopicDistribution('C', {
+    const isotopicDistribution = new IsotopicDistribution('C', {
       ionizations: 'H+',
     });
-    let distribution = isotopicDistribution.getDistribution();
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([
       { x: 13.00727645232093, y: 0.9891862305 },
       { x: 14.010662031727025, y: 0.010812539 },
@@ -198,18 +198,18 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of C10 and getXY', () => {
-    let isotopicDistribution = new IsotopicDistribution('C10');
-    let xy = isotopicDistribution.getXY();
+    const isotopicDistribution = new IsotopicDistribution('C10');
+    const xy = isotopicDistribution.getXY();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.99999);
     expect(xy.x[0]).toBe(120);
     expect(xy.y[0]).toBe(100);
   });
 
   it('create distribution of C10 and getPeaks', () => {
-    let isotopicDistribution = new IsotopicDistribution('C10', {
+    const isotopicDistribution = new IsotopicDistribution('C10', {
       fwhm: 0,
     });
-    let peaks = isotopicDistribution.getPeaks({ maxValue: 1 });
+    const peaks = isotopicDistribution.getPeaks({ maxValue: 1 });
     expect(peaks[0]).toStrictEqual({
       x: 120,
       y: 1,
@@ -222,28 +222,28 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of C10 and getXY with sumValue to 100', () => {
-    let isotopicDistribution = new IsotopicDistribution('C10');
-    let xy = isotopicDistribution.getXY({ sumValue: 100 });
+    const isotopicDistribution = new IsotopicDistribution('C10');
+    const xy = isotopicDistribution.getXY({ sumValue: 100 });
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.99999);
     expect(xy.x[0]).toBe(120);
     expect(xy.y.reduce((previous, current) => previous + current, 0)).toBe(100);
   });
 
   it('create distribution of C10 with threshold', () => {
-    let isotopicDistribution = new IsotopicDistribution('C10', {
+    const isotopicDistribution = new IsotopicDistribution('C10', {
       threshold: 0.1,
     });
-    let xy = isotopicDistribution.getXY({ sumValue: 100 });
+    const xy = isotopicDistribution.getXY({ sumValue: 100 });
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.95);
     expect(xy.x[0]).toBe(120);
     expect(xy.y.reduce((previous, current) => previous + current, 0)).toBe(100);
   });
 
   it('create distribution of C10 with limit', () => {
-    let isotopicDistribution = new IsotopicDistribution('C10', {
+    const isotopicDistribution = new IsotopicDistribution('C10', {
       limit: 2,
     });
-    let xy = isotopicDistribution.getXY();
+    const xy = isotopicDistribution.getXY();
     expect(xy).toBeDeepCloseTo({
       x: [120, 121.00335483507],
       y: [100, 10.815728292732235],
@@ -251,24 +251,33 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of C10 and getVariables with maxValue to 100', () => {
-    let isotopicDistribution = new IsotopicDistribution('C10');
-    let variables = isotopicDistribution.getVariables();
+    const isotopicDistribution = new IsotopicDistribution('C10');
+    const variables = isotopicDistribution.getVariables();
     expect(variables.x.data.length).toBe(6);
     expect(Math.max(...variables.y.data)).toBe(100);
   });
 
+  it('C1000000', () => {
+    const isotopicDistribution = new IsotopicDistribution('C1000000');
+    const variables = isotopicDistribution.getVariables();
+    expect(variables.x.data.length).toBe(822);
+    expect(Math.max(...variables.y.data)).toBe(100);
+    const spectrum = isotopicDistribution.getGaussian();
+    expect(spectrum.x).toHaveLength(28950);
+  });
+
   it('create distribution of Ru5 and getXY', () => {
-    let isotopicDistribution = new IsotopicDistribution('Ru5');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('Ru5');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.99999);
     distribution.maxToOne();
-    let element = distribution.array[25];
+    const element = distribution.array[25];
     expect(element).toStrictEqual({ x: 505.52516825500203, y: 1 });
   });
 
   it('create distribution of empty array and getXY', () => {
-    let isotopicDistribution = new IsotopicDistribution([]);
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution([]);
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeNaN();
     distribution.maxToOne();
     expect(isotopicDistribution.getXY()).toStrictEqual({ x: [], y: [] });
@@ -278,8 +287,8 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of C1000H1000', () => {
-    let isotopicDistribution = new IsotopicDistribution('C1000H1000');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C1000H1000');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.99999);
     distribution.maxToOne();
     distribution.sortY();
@@ -290,8 +299,8 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of C1000H1000N1000', () => {
-    let isotopicDistribution = new IsotopicDistribution('C1000H1000N1000');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C1000H1000N1000');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.99999);
     distribution.maxToOne();
     distribution.sortY();
@@ -302,8 +311,8 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution of Ala1000', () => {
-    let isotopicDistribution = new IsotopicDistribution('Ala1000');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('Ala1000');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.9999);
     distribution.maxToOne();
     distribution.sortY();
@@ -311,45 +320,45 @@ describe('test isotopicDistribution', () => {
   });
 
   it('create distribution with charged molecule C+', () => {
-    let isotopicDistribution = new IsotopicDistribution('C+');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C+');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.9999);
     expect(distribution.array[0].x).toBeCloseTo(11.99945, 5);
   });
   it('create distribution with charged molecule C2(+2)', () => {
-    let isotopicDistribution = new IsotopicDistribution('C2(+2)');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C2(+2)');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.9999);
     expect(distribution.array[0].x).toBeCloseTo(11.99945, 5);
   });
   it('create distribution with charged molecule C2(-2)', () => {
-    let isotopicDistribution = new IsotopicDistribution('C2(-2)');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('C2(-2)');
+    const distribution = isotopicDistribution.getDistribution();
     expect(isotopicDistribution.confidence).toBeGreaterThan(0.9999);
     expect(distribution.array[0].x).toBeCloseTo(12.00055, 5);
   });
 
   it('create distribution with no elements Ru0', () => {
-    let isotopicDistribution = new IsotopicDistribution('Ru0');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('Ru0');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array).toMatchObject([{ x: 0, y: 1 }]);
   });
 
   it('create distribution with null elements', () => {
-    let isotopicDistribution = new IsotopicDistribution('CRu0C');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('CRu0C');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution.array[0].x).toBe(24);
   });
 
   it('distribution of CH-1', () => {
-    let isotopicDistribution = new IsotopicDistribution('CH-1');
-    let distribution = isotopicDistribution.getDistribution();
+    const isotopicDistribution = new IsotopicDistribution('CH-1');
+    const distribution = isotopicDistribution.getDistribution();
     expect(distribution).toStrictEqual({ array: [] });
   });
 
   it('gaussian of N(H-1)', () => {
-    let isotopicDistribution = new IsotopicDistribution('NH-1');
-    let distribution = isotopicDistribution.getGaussian();
+    const isotopicDistribution = new IsotopicDistribution('NH-1');
+    const distribution = isotopicDistribution.getGaussian();
     expect(distribution).toStrictEqual({ x: [], y: [] });
   });
 });
