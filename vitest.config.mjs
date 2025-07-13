9af7f27db65368a70a98ce4f209c0b4da2551ce8
Michaël Zasso <targos@protonmail.com>
2025-07-13 15:50:35 +0200

    test: use istanbul for faster coverage

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,6 +3,7 @@ import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      provider: 'istanbul',
       exclude: [
         'benchmark/**',
         'examples/**',
