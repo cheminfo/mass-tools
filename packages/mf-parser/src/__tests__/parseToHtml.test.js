f4f3db9af73421efeb775a0dc8b835b6d1fd44d1
Luc Patiny <luc@patiny.com>
2026-03-04 13:43:26 +0100

    test: add parseToHtml of range

--- packages/mf-parser/src/__tests__/parseToHtml.test.js
+++ packages/mf-parser/src/__tests__/parseToHtml.test.js
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest';
 
 import { parseToHtml } from '..';
 
-test('parseToHtml', () => {
-  let result = parseToHtml('SO4(--)');
+test('SO4(--)', () => {
+  const result = parseToHtml('SO4(--)');
 
   expect(result).toBe(
     'SO<span style="flex-direction: column;display: inline-flex;justify-content: center;text-align: left;vertical-align: middle;"><sup style="line-height: 1; font-size: 70%">-2</sup><sub style="line-height: 1; font-size: 70%">4</sub></span>',
@@ -12,4 +12,10 @@ test('parseToHtml', () => {
   let result2 = parseToHtml('12.2123C . 0.4123O . 32.6123N');
 
   expect(result2).toBe('12.2123C  •  0.4123O  •  32.6123N');
+});
+
+test('C1-3H+', () => {
+  const result = parseToHtml('C1-3H+');
+
+  expect(result).toBe('C<sub>1-3</sub>H<sup>+</sup>');
 });
