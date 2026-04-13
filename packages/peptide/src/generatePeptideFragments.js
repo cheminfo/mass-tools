7e1ebfa7b3eba2dc6616a1b3d60811e193339928
Luc Patiny <luc@patiny.com>
2026-04-13 11:12:03 +0200

    chore(peptide): destructure options

--- packages/peptide/src/generatePeptideFragments.js
+++ packages/peptide/src/generatePeptideFragments.js
@@ -1,21 +1,38 @@
-export function generatePeptideFragments(mf, options) {
-  if (options === undefined) {
-    options = {
-      a: false,
-      b: true,
-      c: false,
-      x: false,
-      y: true,
-      z: false,
-      i: false,
-      ya: false,
-      yb: false,
-      yc: false,
-      zc: false,
-    };
-  }
-  options.maxInternal = options.maxInternal || Number.MAX_VALUE;
-  options.minInternal = options.minInternal || 0;
+/**
+ * Generate peptide fragments from a molecular formula.
+ * @param {string} mf - The molecular formula to fragment.
+ * @param {object} [options={}] - Fragmentation options.
+ * @param {boolean} [options.a=false] - Generate a-type fragments.
+ * @param {boolean} [options.b=true] - Generate b-type fragments.
+ * @param {boolean} [options.c=false] - Generate c-type fragments.
+ * @param {boolean} [options.x=false] - Generate x-type fragments.
+ * @param {boolean} [options.y=true] - Generate y-type fragments.
+ * @param {boolean} [options.z=false] - Generate z-type fragments.
+ * @param {boolean} [options.i=false] - Generate internal fragments.
+ * @param {boolean} [options.ya=false] - Generate ya-type double fragmentation.
+ * @param {boolean} [options.yb=false] - Generate yb-type double fragmentation.
+ * @param {boolean} [options.yc=false] - Generate yc-type double fragmentation.
+ * @param {boolean} [options.zc=false] - Generate zc-type double fragmentation.
+ * @param {number} [options.maxInternal=Infinity] - Maximum number of residues for internal fragments.
+ * @param {number} [options.minInternal=0] - Minimum number of residues for internal fragments.
+ * @returns {string[]} Array of fragment molecular formulas.
+ */
+export function generatePeptideFragments(mf, options = {}) {
+  const {
+    a = false,
+    b = true,
+    c = false,
+    x = false,
+    y = true,
+    z = false,
+    i: internal = false,
+    ya = false,
+    yb = false,
+    yc = false,
+    zc = false,
+    maxInternal = Number.MAX_VALUE,
+    minInternal = 0,
+  } = options;
 
   let mfs = [];
   // need to allow 0-9 to deal with neutral loss
@@ -39,23 +56,23 @@ export function generatePeptideFragments(mf, options) {
   for (let i = 1; i < mfparts.length; i++) {
     nTerm += mfparts[i - 1];
     cTerm = mfparts[mfparts.length - i] + cTerm;
-    addNTerm(mfs, nTerm, i, options);
-    addCTerm(mfs, cTerm, i, options);
-    if (options.i) mfs.push(`${mfparts[i]}HC-1O-1(+1)$i:${mfparts[i]}`);
+    addNTerm(mfs, nTerm, i, a, b, c);
+    addCTerm(mfs, cTerm, i, x, y, z);
+    if (internal) mfs.push(`${mfparts[i]}HC-1O-1(+1)$i:${mfparts[i]}`);
 
-    if (options.ya || options.yb || options.yc || options.zc) {
+    if (ya || yb || yc || zc) {
       // we have double fragmentations
       for (
         let j = i + 1;
-        j < Math.min(mfparts.length, options.maxInternal + i + 1);
+        j < Math.min(mfparts.length, maxInternal + i + 1);
         j++
       ) {
         let iTerm = '';
-        if (j - i >= options.minInternal) {
+        if (j - i >= minInternal) {
           for (let k = i; k < j; k++) {
             iTerm += mfparts[k];
           }
-          addITerm(mfs, iTerm, mfparts.length - i, j, options);
+          addITerm(mfs, iTerm, mfparts.length - i, j, ya, yb, yc, zc);
         }
       }
     }
@@ -69,21 +86,21 @@ export function generatePeptideFragments(mf, options) {
   return mfs;
 }
 
-function addNTerm(mfs, nTerm, i, options) {
-  if (options.a) mfs.push(`${nTerm}C-1O-1(+1)$a${i}`);
-  if (options.b) mfs.push(`${nTerm}(+1)$b${i}`);
-  if (options.c) mfs.push(`${nTerm}NH3(+1)$c${i}`);
+function addNTerm(mfs, nTerm, i, a, b, c) {
+  if (a) mfs.push(`${nTerm}C-1O-1(+1)$a${i}`);
+  if (b) mfs.push(`${nTerm}(+1)$b${i}`);
+  if (c) mfs.push(`${nTerm}NH3(+1)$c${i}`);
 }
 
-function addITerm(mfs, iTerm, i, j, options) {
-  if (options.ya) mfs.push(`H${iTerm}C-1O-1(+1)$a${j}y${i}`);
-  if (options.yb) mfs.push(`H${iTerm}(+1)$b${j}y${i}`);
-  if (options.yc) mfs.push(`H${iTerm}NH3(+1)$c${j}y${i}`);
-  if (options.zc) mfs.push(`N-1${iTerm}NH3(+1)$c${j}z${i}`);
+function addITerm(mfs, iTerm, i, j, ya, yb, yc, zc) {
+  if (ya) mfs.push(`H${iTerm}C-1O-1(+1)$a${j}y${i}`);
+  if (yb) mfs.push(`H${iTerm}(+1)$b${j}y${i}`);
+  if (yc) mfs.push(`H${iTerm}NH3(+1)$c${j}y${i}`);
+  if (zc) mfs.push(`N-1${iTerm}NH3(+1)$c${j}z${i}`);
 }
 
-function addCTerm(mfs, cTerm, i, options) {
-  if (options.x) mfs.push(`CO(+1)${cTerm}$x${i}`);
-  if (options.y) mfs.push(`H2(+1)${cTerm}$y${i}`);
-  if (options.z) mfs.push(`N-1H-1(+1)${cTerm}$z${i}`);
+function addCTerm(mfs, cTerm, i, x, y, z) {
+  if (x) mfs.push(`CO(+1)${cTerm}$x${i}`);
+  if (y) mfs.push(`H2(+1)${cTerm}$y${i}`);
+  if (z) mfs.push(`N-1H-1(+1)${cTerm}$z${i}`);
 }
