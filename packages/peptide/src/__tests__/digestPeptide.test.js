01e970182a9c2a3b9463a23b273ac5a194a252ab
Luc Patiny <luc@patiny.com>
2026-04-13 15:02:48 +0200

    feat(peptide): add minDigestions and maxDigestions

--- packages/peptide/src/__tests__/digestPeptide.test.js
+++ packages/peptide/src/__tests__/digestPeptide.test.js
@@ -11,9 +11,9 @@ describe('Checking digest sequence', () => {
     });
 
     expect(result).toStrictEqual([
+      'HAlaAlaLysOH$D3>5',
       'HLysOH$D1>1',
       'HLysOH$D2>2',
-      'HAlaAlaLysOH$D3>5',
     ]);
   });
 
@@ -25,11 +25,11 @@ describe('Checking digest sequence', () => {
     });
 
     expect(result).toStrictEqual([
-      'HLysOH$D1>1',
-      'HLysLysOH$D1>2',
-      'HLysOH$D2>2',
-      'HLysAlaAlaLysOH$D2>5',
       'HAlaAlaLysOH$D3>5',
+      'HLysAlaAlaLysOH$D2>5',
+      'HLysLysOH$D1>2',
+      'HLysOH$D1>1',
+      'HLysOH$D2>2',
     ]);
   });
 
@@ -40,13 +40,13 @@ describe('Checking digest sequence', () => {
       maxMissed: 1,
     });
 
-    expect(result).toStrictEqual(['HLysLysOH$D1>2', 'HLysAlaAlaLysOH$D2>5']);
+    expect(result).toStrictEqual(['HLysAlaAlaLysOH$D2>5', 'HLysLysOH$D1>2']);
   });
 
   it('Normal small sequence digest, default value', () => {
     let result = digestPeptide('HLysAlaOH');
 
-    expect(result).toStrictEqual(['HLysOH$D1>1', 'HAlaOH$D2>2']);
+    expect(result).toStrictEqual(['HAlaOH$D2>2', 'HLysOH$D1>1']);
   });
 
   //  Leu, Phe, Val, Ile, Ala, Met
@@ -58,8 +58,8 @@ describe('Checking digest sequence', () => {
     });
 
     expect(result).toStrictEqual([
-      'HLysOH$D1>1',
       'HLeuProOH$D2>3',
+      'HLysOH$D1>1',
       'HValOH$D4>4',
     ]);
   });
@@ -72,8 +72,135 @@ describe('Checking digest sequence', () => {
     });
 
     expect(result).toStrictEqual([
-      'HLysLeu(H-1OH)OH$D1>2',
       'HLeu(H-1OH)ValOH$D2>3',
+      'HLysLeu(H-1OH)OH$D1>2',
     ]);
+  });
+
+  it('HLysLysAlaAlaLysOH with maxDigestions=1 (single hydrolysis)', () => {
+    let result = digestPeptide('HLysLysAlaAlaLysOH', {
+      enzyme: 'trypsin',
+      minMissed: 0,
+      maxMissed: 0,
+      maxDigestions: 1,
+    });
+
+    // With 2 possible cleavage sites, choosing 1 gives us 2 combinations:
+    // - Cleave after 1st Lys: HLysOH + HLysAlaAlaLysOH
+    // - Cleave after 2nd Lys: HLysLysOH + HAlaAlaLysOH
+    expect(result).toStrictEqual([
+      'HAlaAlaLysOH$D3>5',
+      'HLysAlaAlaLysOH$D2>5',
+      'HLysLysOH$D1>2',
+      'HLysOH$D1>1',
+    ]);
+  });
+
+  it('HLysLysAlaAlaLysOH with maxDigestions=2', () => {
+    let result = digestPeptide('HLysLysAlaAlaLysOH', {
+      enzyme: 'trypsin',
+      minMissed: 0,
+      maxMissed: 0,
+      maxDigestions: 2,
+    });
+
+    expect(result).toStrictEqual([
+      'HAlaAlaLysOH$D3>5',
+      'HLysOH$D1>1',
+      'HLysOH$D2>2',
+    ]);
+  });
+
+  it('HLysLysAlaAlaLysOH with minDigestions=2', () => {
+    let result = digestPeptide('HLysLysAlaAlaLysOH', {
+      enzyme: 'trypsin',
+      minMissed: 0,
+      maxMissed: 0,
+      minDigestions: 2,
+    });
+
+    expect(result).toStrictEqual([
+      'HAlaAlaLysOH$D3>5',
+      'HLysOH$D1>1',
+      'HLysOH$D2>2',
+    ]);
+  });
+
+  it('HLysLysAlaAlaLysOH with minDigestions=2, maxDigestions=2', () => {
+    let result = digestPeptide('HLysLysAlaAlaLysOH', {
+      enzyme: ' trypsin',
+      minMissed: 0,
+      maxMissed: 0,
+      minDigestions: 2,
+      maxDigestions: 2,
+    });
+
+    expect(result).toStrictEqual([
+      'HAlaAlaLysOH$D3>5',
+      'HLysOH$D1>1',
+      'HLysOH$D2>2',
+    ]);
+  });
+
+  it('HLysLysAlaAlaLysOH with minDigestions=5 (impossible)', () => {
+    let result = digestPeptide('HLysLysAlaAlaLysOH', {
+      enzyme: 'trypsin',
+      minMissed: 0,
+      maxMissed: 0,
+      minDigestions: 5,
+    });
+
+    expect(result).toStrictEqual([]);
+  });
+
+  it('HLysLysAlaAlaLysOH with maxDigestions=1 and maxMissed=1', () => {
+    let result = digestPeptide('HLysLysAlaAlaLysOH', {
+      enzyme: 'trypsin',
+      minMissed: 0,
+      maxMissed: 1,
+      maxDigestions: 1,
+    });
+
+    // With maxDigestions=1, we get 2 combinations, each can have missed cleavages:
+    // Combination 1 (cleave after 1st Lys): HLysOH, HLysAlaAlaLysOH, HLysLysAlaAlaLysOH (with missed)
+    // Combination 2 (cleave after 2nd Lys): HLysLysOH, HAlaAlaLysOH, HLysLysAlaAlaLysOH (with missed)
+    expect(result).toStrictEqual([
+      'HAlaAlaLysOH$D3>5',
+      'HLysAlaAlaLysOH$D2>5',
+      'HLysLysAlaAlaLysOH$D1>5',
+      'HLysLysOH$D1>2',
+      'HLysOH$D1>1',
+    ]);
+  });
+
+  it('HLysAlaValOH with enzyme any and maxDigestions=1', () => {
+    let result = digestPeptide('HLysAlaValOH', {
+      enzyme: 'any',
+      minMissed: 0,
+      maxMissed: 0,
+      maxDigestions: 1,
+    });
+
+    // With 2 possible cleavage sites (after Lys and after Ala), choosing 1 gives 2 combinations:
+    // - Cleave after Lys: HLysOH + HAlaValOH
+    // - Cleave after Ala: HLysAlaOH + HValOH
+    expect(result).toStrictEqual([
+      'HAlaValOH$D2>3',
+      'HLysAlaOH$D1>2',
+      'HLysOH$D1>1',
+      'HValOH$D3>3',
+    ]);
+  });
+
+  it('HLysAlaValOH with enzyme any and maxDigestions=2', () => {
+    let result = digestPeptide('HLysAlaValOH', {
+      enzyme: 'any',
+      minMissed: 0,
+      maxMissed: 0,
+      maxDigestions: 2,
+    });
+
+    // With 2 possible cleavage sites, maxDigestions=2 uses all cleavages (default behavior)
+    expect(result).toStrictEqual(['HAlaOH$D2>2', 'HLysOH$D1>1', 'HValOH$D3>3']);
   });
 });
--- packages/peptide/src/digestPeptide.js
+++ packages/peptide/src/digestPeptide.js
@@ -1,3 +1,5 @@
+import { getCombinationsIterator } from 'ml-spectra-processing';
+
 /**
  * Digest a peptide sequence using a specified enzyme.
  * @param {string} sequence - The peptide sequence to digest.
@@ -7,6 +9,8 @@
  * @param {number} [options.maxMissed=0] - Maximum number of missed cleavages.
  * @param {number} [options.minResidue=0] - Minimum number of residues in a fragment.
  * @param {number} [options.maxResidue=Infinity] - Maximum number of residues in a fragment.
+ * @param {number} [options.minDigestions=0] - Minimum number of cleavage sites to use. Generates all combinations with at least this many cleavages. Defaults to `0`.
+ * @param {number} [options.maxDigestions=Infinity] - Maximum number of cleavage sites to use. Generates all combinations with at most this many cleavages. Defaults to `Infinity`.
  * @returns {string[]} Array of digested peptide fragments.
  */
 export function digestPeptide(sequence, options = {}) {
@@ -16,52 +20,162 @@ export function digestPeptide(sequence, options = {}) {
     maxMissed = 0,
     minResidue = 0,
     maxResidue = Number.MAX_VALUE,
+    minDigestions = 0,
+    maxDigestions = Number.MAX_VALUE,
   } = options;
 
   sequence = sequence.replace(/^H([^a-z])/, '$1').replace(/OH$/, '');
   let regexp = getRegexp(enzyme);
-  let fragments = sequence.replace(regexp, '$1 ').split(/ /).filter(Boolean);
+  let allFragments = sequence.replace(regexp, '$1 ').split(/ /).filter(Boolean);
 
-  {
-    let from = 0;
-    for (let i = 0; i < fragments.length; i++) {
-      let nbResidue = fragments[i]
+  const numCleavages = allFragments.length - 1;
+
+  // Return empty if minDigestions is impossible to achieve
+  if (minDigestions > numCleavages) {
+    return [];
+  }
+
+  const effectiveMaxDigestions = Math.min(maxDigestions, numCleavages);
+  const effectiveMinDigestions = minDigestions;
+
+  if (effectiveMinDigestions > effectiveMaxDigestions) {
+    return [];
+  }
+
+  const allResults = new Set();
+
+  // Determine if we need combinatorial approach
+  // Use combinatorial logic when maxDigestions explicitly limits cleavages OR minDigestions > 0
+  const useCombinatorial = maxDigestions < numCleavages || minDigestions > 0;
+
+  // When using combinatorial due to maxDigestions limit, ensure at least 1 cleavage
+  const startDigestions = useCombinatorial
+    ? Math.max(1, effectiveMinDigestions)
+    : effectiveMinDigestions;
+
+  if (!useCombinatorial) {
+    // Default behavior: use all available cleavage sites
+    const fragments = [];
+    let fragmentStart = 0;
+    for (let i = 0; i < allFragments.length; i++) {
+      let nbResidue = allFragments[i]
         .replaceAll(/([A-Z][a-z]{2})/g, ' $1')
         .split(/ /)
         .filter(Boolean).length;
-      fragments[i] = {
-        sequence: fragments[i],
+      fragments.push({
+        sequence: allFragments[i],
+        nbResidue,
+        from: fragmentStart,
+        to: fragmentStart + nbResidue - 1,
+      });
+      fragmentStart += nbResidue;
+    }
+
+    for (let i = 0; i < fragments.length - minMissed; i++) {
+      for (
+        let j = minMissed;
+        j <= Math.min(maxMissed, fragments.length - i - 1);
+        j++
+      ) {
+        let fragment = '';
+        let nbResidue = 0;
+        for (let k = i; k <= i + j; k++) {
+          fragment += fragments[k].sequence;
+          nbResidue += fragments[k].nbResidue;
+        }
+        let from = fragments[i].from + 1;
+        let to = fragments[i + j].to + 1;
+        if (fragment && nbResidue >= minResidue && nbResidue <= maxResidue) {
+          allResults.add(`H${fragment}OH$D${from}>${to}`);
+        }
+      }
+    }
+  } else {
+    // Combinatorial approach: generate all combinations of cleavage sites
+    for (
+      let numDigestions = startDigestions;
+      numDigestions <= effectiveMaxDigestions;
+      numDigestions++
+    ) {
+      const combinations = getCombinationsIterator(numCleavages, numDigestions);
+
+      for (const cleavageSites of combinations) {
+        // Create fragments based on selected cleavage sites
+        const fragments = createFragmentsFromCleavages(
+          allFragments,
+          cleavageSites,
+        );
+
+        // Generate results with missed cleavages
+        for (let i = 0; i < fragments.length - minMissed; i++) {
+          for (
+            let j = minMissed;
+            j <= Math.min(maxMissed, fragments.length - i - 1);
+            j++
+          ) {
+            let fragment = '';
+            let nbResidue = 0;
+            for (let k = i; k <= i + j; k++) {
+              fragment += fragments[k].sequence;
+              nbResidue += fragments[k].nbResidue;
+            }
+            let from = fragments[i].from + 1;
+            let to = fragments[i + j].to + 1;
+            if (
+              fragment &&
+              nbResidue >= minResidue &&
+              nbResidue <= maxResidue
+            ) {
+              allResults.add(`H${fragment}OH$D${from}>${to}`);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return [...allResults].toSorted();
+}
+
+/**
+ * Create fragments from selected cleavage sites.
+ * @param {string[]} allFragments - All possible fragments if all sites were cleaved.
+ * @param {number[]} cleavageSites - Indices of cleavage sites to use.
+ * @returns {Array<{sequence: string, nbResidue: number, from: number, to: number}>} Fragment objects.
+ */
+function createFragmentsFromCleavages(allFragments, cleavageSites) {
+  const fragments = [];
+  const cleavageSet = new Set(cleavageSites);
+
+  let currentFragment = '';
+  let fragmentStartIndex = 0;
+
+  for (let i = 0; i < allFragments.length; i++) {
+    currentFragment += allFragments[i];
+
+    // If this is a cleavage site or the last fragment, close the current fragment
+    if (cleavageSet.has(i) || i === allFragments.length - 1) {
+      const nbResidue = currentFragment
+        .replaceAll(/([A-Z][a-z]{2})/g, ' $1')
+        .split(/ /)
+        .filter(Boolean).length;
+
+      const from = fragmentStartIndex;
+      const to = from + nbResidue - 1;
+
+      fragments.push({
+        sequence: currentFragment,
         nbResidue,
         from,
-        to: from + nbResidue - 1,
-      };
-      from += nbResidue;
+        to,
+      });
+
+      fragmentStartIndex = to + 1;
+      currentFragment = '';
     }
   }
 
-  let results = [];
-
-  for (let i = 0; i < fragments.length - minMissed; i++) {
-    for (
-      let j = minMissed;
-      j <= Math.min(maxMissed, fragments.length - i - 1);
-      j++
-    ) {
-      let fragment = '';
-      let nbResidue = 0;
-      for (let k = i; k <= i + j; k++) {
-        fragment += fragments[k].sequence;
-        nbResidue += fragments[k].nbResidue;
-      }
-      let from = fragments[i].from + 1;
-      let to = fragments[i + j].to + 1;
-      if (fragment && nbResidue >= minResidue && nbResidue <= maxResidue) {
-        results.push(`H${fragment}OH$D${from}>${to}`);
-      }
-    }
-  }
-
-  return results;
+  return fragments;
 }
 
 function getRegexp(enzyme) {
